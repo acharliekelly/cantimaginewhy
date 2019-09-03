292969694d5e0a72ee0e3dec4ca748baa7d6cd13
Charlie Kelly <charliekelly31415@gmail.com>
2019-09-03 01:14:31 -0400

    set defaults

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
 import Gallery from './components/Gallery';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <Gallery currentAlbum="landscape" />
-        
-      </header>
+      <Gallery />
     </div>
   );
 }

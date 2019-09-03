79276dbeeaeb57dd57b051aa1206617db990073d
Charlie Kelly <charliekelly31415@gmail.com>
2019-09-03 01:15:32 -0400

    fixed main component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,109 +2,103 @@ import React, { Component } from 'react';
 import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
 import axios from 'axios';
 import albumList from '../utils/albums';
+import './list.css';
+import './gallery.css';
 
 class Gallery extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      isLoaded: false,
       pictures: [],
       selectedAlbum: null
     };
   }
 
   componentDidMount () {
-    if (this.props.currentAlbum) {
-      this.getTaggedPictures(this.props.currentAlbum);
+    const { currentAlbum } = this.props;
+    if (currentAlbum) {
+      console.log('Loaded: ', currentAlbum);
+      this.updateGallery(currentAlbum);
     }
     
-  }
-
-  getTaggedPictures = tagName => {
-    // load images
-    axios.get(`https://res.cloudinary.com/cantimaginewhy/image/list/${tagName}.json`)
-      .then(res => {
-        this.setState({
-          selectedAlbum: tagName, 
-          pictures: res.data.resources,
-          isLoaded: true
-        });
-      });
   }
 
   handleAlbumSelect = event => {
-    const tagName = event.target.id
-    this.getTaggedPictures(tagName);
-
+    const tagName = event.target.id;
+    console.log('Selected album: ', tagName);
+    this.updateGallery(tagName);
   }
 
-  render () {
-    const { isLoaded, pictures } = this.state;
-    if (!isLoaded) {
-      this.getTaggedPictures(this.props.currentAlbum);
-      return 'Loading...';
-    } else {
-      return (
-        <div className="main">
-          <div className="album-list">
-              <h2>Select Album</h2>
-              {/* Takes data from src/config/albums.js to generate album list */}
-              {albumList.albums.map(album => {
-                  return (
-                      <div key={album.tag} id={album.tag} className="album-btn" onClick={this.handleAlbumSelect}>
-                          <Image  
-                              publicId={`art/${album.thumbnail}`}
-                              className="thumbnail inline"
-                              width="150"
-                              height="150"
-                              crop="fit"
-                              quality="80"
-                          >
-                              <Transformation quality="auto" fetchFormat="auto" />
-                          </Image>
-                          <h3 className="album-name">{album.name}</h3>
-                      </div>
-                  );
-              })}
-              </div>
-            <h1>Gallery</h1>
-            <div className="gallery">
-                <CloudinaryContext cloudName="cantimaginewhy">
-                    {
-                        pictures.map(picture => {
-                            return (
-                                <div className="responsive" key={picture.public_id}>
-                                    <div className="img">
-                                        <a 
-                                          target="_blank" 
-                                          rel="noopener noreferrer" 
-                                          href={`https://res.cloudinary.com/cantimaginewhy/${picture.public_id}.jpg`}>
-                                            <Image publicId={picture.public_id}>
-                                                <Transformation
-                                                    crop="scale"
-                                                    width="300"
-                                                    height="200"
-                                                    dpr="auto"
-                                                    responsive_placeholder="blank"
-                                                />
-                                            </Image>
-                                        </a>
-                                        <div className="title">{picture.context.custom.caption}</div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </CloudinaryContext>
-                <div className="clearfix"></div>
-            </div>
-        </div>
-
-    );
-    }
+  updateGallery = tagName => {
+    // load images
+    const url = `https://res.cloudinary.com/cantimaginewhy/image/list/${tagName}.json`;
+    axios.get(url)
+      .then(res => {
+        this.setState({
+          selectedAlbum: tagName, 
+          pictures: res.data.resources
+        });
+      });
+  }
     
-  }
-
+  render () {
+    const { pictures, selectedAlbum } = this.state;
+    return (
+        <CloudinaryContext cloudName="cantimaginewhy">
+          <header>
+            <h2 className="list-title">Select Gallery</h2>
+            <div className="album-list">
+              {albumList.albums.map(album => (
+                    <div key={album.tag} className="album-btn" onClick={this.handleAlbumSelect}>
+                        <Image  
+                            id={album.tag}
+                            publicId={`art/${album.thumbnail}`}
+                            className="thumbnail inline"
+                            width="150"
+                            height="150"
+                            crop="fit"
+                            quality="80"
+                        >
+                            <Transformation quality="auto" fetchFormat="auto" />
+                        </Image>
+                        <h3 className="album-name">{album.name}</h3>
+                    </div>
+                  )) }
+              </div>
+          </header>
+          <main>
+            <h1 className="gallery-title">{selectedAlbum}</h1>
+            <div className="gallery">
+              {pictures.map(picture => {
+                let caption = 'Untitled';
+                if (picture.context && picture.context.custom && picture.context.custom.caption) {
+                  caption = picture.context.custom.caption;
+                }
+                return (
+                  <div className="responsive img" key={picture.public_id}>
+                      <a 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        href={`https://res.cloudinary.com/cantimaginewhy/${picture.public_id}.jpg`}>
+                          <Image cloudName="cantimaginewhy" publicId={picture.public_id}>
+                              <Transformation
+                                  crop="scale"
+                                  width="300"
+                                  height="200"
+                                  dpr="auto"
+                                  responsive_placeholder="blank"
+                              />
+                          </Image>
+                      </a>
+                      <div className="title">{caption}</div>
+                  </div>
+                  )
+              }) }
+            </div>
+        </main>
+        </CloudinaryContext>
+      );
+    }
 }
 
 export default Gallery;

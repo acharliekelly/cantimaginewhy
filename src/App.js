23db8aad7a35aea42414b1724f6b2943a4c9e9f7
Charlie Kelly <charliekelly31415@gmail.com>
2019-09-18 16:23:32 -0400

    add router and navs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import Gallery from './components/Gallery';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+// import Nav from 'react-bootstrap/Nav';
+
 import './App.css';
+import './css/main.css';
+
+// import Menu from './components/Menu';
+import Home from './components/Home';
+import AboutPage from './components/About';
+import Gallery from './components/Gallery';
+
+const ContactPage = () => (
+  <div>
+    <header>Contact</header>
+    <main className="todo">Coming Soon</main>
+  </div>
+);
 
 const App = () => {
   return (
-    <div className="App">
-      <Gallery />
-    </div>
+    <Router>
+      <nav className="menu">
+        <ul>
+          <li>
+            <NavLink to="/home">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/gallery">Gallery</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+        
+      </nav>
+      <div className="content">
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/gallery" component={Gallery} />
+      </div>
+      
+    </Router>
   );
 }
 

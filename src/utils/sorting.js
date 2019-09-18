0c88c932e59e3a9f228cb5fa9b63a7a4fa72ae75
Charlie Kelly <charliekelly31415@gmail.com>
2019-09-18 16:24:49 -0400

    JSON file for tag filters

---
+++ src/utils/sorting.js
@@ -0,0 +1,107 @@
+
+export const albums = [
+  {
+    "name": "Landscapes",
+    "tag": "landscape",
+    "thumbnail": "wachusett-reservoir"
+  },
+  {
+    "name": "Monogram",
+    "tag": "monogram",
+    "thumbnail": "ak-blue-heart-monogram"
+  },
+  {
+    "name": "Illuminations",
+    "tag": "nameplate",
+    "thumbnail": "jennylynne_2012"
+  },
+  {
+    "name": "Holiday Cards",
+    "tag": "holiday",
+    "thumbnail": "valentines-card-2012"
+  },
+  {
+    "name": "En Plein Air",
+    "tag": "en plein air",
+    "thumbnail": "watertown_footbridge-fs"
+  }
+];
+
+export const media = [
+  {
+    "name": "Pencil",
+    "tag": "graphite",
+    "thumbnail": "winter-house"
+  },
+  {
+    "name": "Colored Pencil",
+    "tag": "colored pencil",
+    "thumbnail": "winter-yosemite"
+  },
+  {
+    "name": "Watercolor Pencil",
+    "tag": "watercolor pencil",
+    "thumbnail": "leaving-star-island_2015"
+  },
+  {
+    "name": "Pastel",
+    "tag": "pastel",
+    "thumbnail": "rainbow-end_2017"
+  },
+  {
+    "name": "Watercolor",
+    "tag": "watercolor",
+    "thumbnail": "ck_ice"
+  },
+  {
+    "name": "Acrylic",
+    "tag": "acrylic",
+    "thumbnail": "riparian_balcony-fs"
+  }
+];
+
+export const styles = [
+  {
+    "name": "Triptych",
+    "tag": "triptych",
+    "thumbnail": "winter-yosemite"
+  },
+  {
+    "name": "Pointillist",
+    "tag": "pointillist",
+    "thumbnail": "fun-with-dots_2017"
+  },
+  {
+    "name": "Nightscape",
+    "tag": "nightscape",
+    "thumbnail": "cambridge-night_2019"
+  },
+  {
+    "name": "Panoramic",
+    "tag": "panorama",
+    "thumbnail": "winter-moon-2"
+  },
+];
+
+export const seasons = [
+  {
+    "name": "Summer",
+    "tag": "summer",
+    "thumbnail": "salt-pond"
+  },
+  {
+    "name": "Fall",
+    "tag": "autumn",
+    "thumbnail": "autumn-at-mt-feake_2017"
+  },
+  {
+    "name": "Winter",
+    "tag": "winter",
+    "thumbnail": "winter-walk"
+  },
+  {
+    "name": "Spring",
+    "tag": "spring",
+    "thumbnail": "esplanade-sunset_2018"
+  }
+];

9399d3aaf820aba67ca6d4438b6dd11871a2cedd
Charlie Kelly <charliekelly31415@gmail.com>
2019-09-18 16:22:55 -0400

    add placeholder pages

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <div>
+    <header>About</header>
+    <main className="todo">Coming Soon</main>
+  </div>
+);
+
+export default AboutPage;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <header>Can't Imagine Why</header>
+    <div className="todo">Coming Soon</div>
+  </div>
+);
+
+export default Home;

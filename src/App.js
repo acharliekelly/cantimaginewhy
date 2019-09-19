65c76eaf1a611eb242edbb840c6a8c8984fcf8e4
Charlie Kelly <charliekelly31415@gmail.com>
2019-09-18 22:42:04 -0400

    fix urls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-// import Nav from 'react-bootstrap/Nav';
+import Nav from 'react-bootstrap/Nav';
 
-import './App.css';
 import './css/main.css';
 
 // import Menu from './components/Menu';
@@ -11,38 +10,45 @@ import AboutPage from './components/About';
 import Gallery from './components/Gallery';
 
 const ContactPage = () => (
-  <div>
+  <div className="content">
     <header>Contact</header>
-    <main className="todo">Coming Soon</main>
+    <main className="lorem">Coming Soon</main>
   </div>
 );
+
+// because of temporary use of gh-pages
+const makeUrl = path => {
+  const host = window.location.hostname;
+  if (host.includes('github.io')) {
+    // using gh-pages, make relative path
+    return '/cantimaginewhy' + path;
+  } else {
+    // otherwise using either localhost or cantimaginewhy.com
+    return path;
+  }
+}
 
 const App = () => {
   return (
     <Router>
-      <nav className="menu">
-        <ul>
-          <li>
-            <NavLink to="/home">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/gallery">Gallery</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-        
-      </nav>
-      <div className="content">
-        <Route path="/home" component={Home} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/gallery" component={Gallery} />
-      </div>
+      <Nav className="menu" activeKey="/home">
+        <Nav.Item>
+          <NavLink to={makeUrl('/home')}>Home</NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink to={makeUrl('/about')}>About</NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink to={makeUrl('/gallery')}>Gallery</NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink to={makeUrl('/contact')}>Contact</NavLink>
+        </Nav.Item>
+      </Nav>
+      <Route path="/home" component={Home} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/contact" component={ContactPage} />
+      <Route path="/gallery" component={Gallery} />
       
     </Router>
   );

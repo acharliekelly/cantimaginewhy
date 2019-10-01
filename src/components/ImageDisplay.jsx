014ccbd1fd79d8223f048d1d556154b27bf6ff4a
Charlie Kelly <charliekelly31415@gmail.com>
2019-10-01 02:41:47 -0400

    change to serve in split screen

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -1,108 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css';
-import { watermarkedImageSrc } from '../utils/imageApi';
+import { variableImageSrc } from '../utils/imageApi';
 
-class ImageDisplay extends Component {
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      currentIndex: 0
-    }
-  }
-
-  componentDidMount () {
-    const { pictures, selectedImage } = this.props;
-    this.setState({
-      currentIndex: pictures.indexOf(selectedImage)
-    });
-  }
-
-  getCaption = index => {
-    const picture = this.props.pictures[index];
-    let caption;
-    try {
-      caption = picture.context.custom.caption;
-    } catch (err) {
-      caption = 'Untitled';
-    }
-    return caption;
-  }
-
-  getImageSource = index => {
-    const pic = this.props.pictures[index];
-    return watermarkedImageSrc(pic.public_id);
-  }
-
-  moveNext = () => {
-    const size = this.props.pictures.length;
-    const idx = this.state.currentIndex;
-    this.setState({
-      currentIndex: (idx + 1) % size
-    });
-  }
-
-  movePrevious = () => {
-    const size = this.props.pictures.length;
-    const idx = this.state.currentIndex;
-    this.setState({
-      currentIndex: (idx + size - 1) % size
-    });
-  }
-
-  showCarousel = () => {
-    const { currentIndex } = this.state;
-    const size = this.props.pictures.length;
-    return (
-      <Lightbox 
-        mainSrc={this.getImageSource(currentIndex)}
-        nextSrc={this.getImageSource((currentIndex + 1) % size)}
-        prevSrc={this.getImageSource((currentIndex + size - 1) % size)}
-        onCloseRequest={this.props.closeImageHandler}
-        onMoveNexRequest={this.moveNext}
-        onMovePrevRequest={this.movePrevious}
-        imageTitle={this.getCaption(currentIndex)}
-      />
-    );
-  }
-
-  showSinglePicture = () => {
-    const picture = this.props.pictures[0];
-
-    return (
-      <Lightbox
-        mainSrc={watermarkedImageSrc(picture.public_id)}
-        onCloseRequest={this.props.closeImageHandler}
-        discourageDownloads={true}
-        imageTitle={this.getCaption(picture)}
-      />
-    );
-  }
-
-  showNoPictures = () => (
-    <div className="no-pictures"></div>
+const ImageDisplay = () => {
+  const { publicId, title, location, medium, size, year, forSale, 
+    origPrice, handleClose } = this.props;
+  return (
+    <div className="image-view">
+      <img alt="" src={variableImageSrc(publicId)} onClick={handleClose} />
+      <div className="image-info">
+        <div className="title">{title} ({year})</div>
+        <div className="info">{location}</div>
+        <div className="info">{medium}, {size}</div>
+        {forSale && (
+          <div className="info">${origPrice}</div>
+        )}
+      </div>
+    </div>
   );
-
-
-  render () {
-    const { pictures, lightboxOpen } = this.props;
-    if (lightboxOpen && pictures.length > 0) {
-      return this.showCarousel();
-    } else {
-      return this.showNoPictures();
-    }
-  }
-
 }
 
 ImageDisplay.propTypes = {
-  pictures: PropTypes.array.isRequired,
-  closeImageHandler: PropTypes.func.isRequired,
-  lightboxOpen: PropTypes.bool.isRequired,
-  selectedImage: PropTypes.object,
-  toolbarButtons: PropTypes.array
+  publicId: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  location: PropTypes.string,
+  medium: PropTypes.string,
+  size: PropTypes.string,
+  year: PropTypes.number,
+  forSale: PropTypes.bool.isRequired,
+  origPrice: PropTypes.number,
+  handleClose: PropTypes.func.isRequired
 };
 
 export default ImageDisplay;

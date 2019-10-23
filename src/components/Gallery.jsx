bfb26760ffadcfe2a23d5b429eb50f540f2cc084
Charlie Kelly <charliekelly31415@gmail.com>
2019-10-23 16:09:53 -0400

    revert to old image view (working)

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Image, CloudinaryContext } from 'cloudinary-react';
 import GalleryNav from './GalleryNav';
-import ImageDisplay from './ImageDisplay';
-import { fetchGallery } from '../utils/imageApi';
-import { purchaseOriginal, purchasePrint, purchasePoster } from '../utils/productApi';
+// import ImageDisplay from './ImageDisplay';
+import { fetchGallery, variableImageSrc } from '../utils/imageApi';
+// import { purchaseOriginal, purchasePrint, purchasePoster } from '../utils/productApi';
 import '../css/gallery.scss';
 
 class Gallery extends Component {
@@ -54,11 +54,48 @@ class Gallery extends Component {
   }
 
   openImageView = picture => {
-    
+    const img = {
+      source: variableImageSrc(picture.public_id, 400),
+      title: this.getPictureCaption(picture),
+      description: this.getPictureProperty(picture, 'alt'),
+      location: this.getPictureProperty(picture, 'location'),
+      medium: this.getPictureProperty(picture, 'medium'),
+      size: this.getPictureProperty(picture, 'size'),
+      year: this.getPictureProperty(picture, 'year'),
+      forSale: (this.getPictureProperty(picture, 'original') === 'available'),
+      forPrint: (this.hasProperty('canvas-id') || this.hasProperty('poster-id')),
+      price: (this.getPictureProperty(picture, 'price', 'NFS')),
+      materialInfo: this.hasProperty('medium') && this.hasProperty('size')
+    }
     this.setState({
       imageViewOpen: true,
-      currentImage: picture
+      currentImage: img
     });
+  }
+
+  getPictureCaption = pictureObj => {
+    return this.getPictureProperty(pictureObj, 'caption', 'Untitled');
+  }
+
+  getPictureProperty = (pictureObj, property, errValue = '') => {
+    let val;
+    try {
+      val = pictureObj.context.custom[property];
+    } catch (err) {
+      val = errValue;
+    }
+    return val;
+  }
+
+  hasProperty = propertyName => {
+    const pictureObj = this.props.currentImage;
+    let val = false;
+    try {
+      val = (pictureObj.context.custom[propertyName] != null)
+    } catch (err) {
+      return false;
+    }
+    return val;
   }
 
   
@@ -101,13 +138,31 @@ class Gallery extends Component {
               </div>
 
               {imageViewOpen && (
-                <ImageDisplay 
-                  currentImage={currentImage}
-                  closeImageView={this.closeImageView} 
-                  purchaseCanvas={purchasePrint}
-                  purchaseOriginal={purchaseOriginal}
-                  purchasePoster={purchasePoster}
-                />
+                <div className="image-view">
+                  <img alt="" src={currentImage.source} onClick={this.closeImageView} />
+                  <div className="image-info">
+                    <div className="title">{currentImage.title}</div>
+                    <div className="info">{currentImage.description}</div>
+                    <div className="info">{currentImage.size}, {currentImage.medium}</div>
+                    {currentImage.forSale && (
+                      <div className="options">
+                        <span className="label">Buy Original:</span>
+                        <span className="purchase buy-orig">${currentImage.price}</span>
+                      </div>
+                    )}
+                    {currentImage.forPrint && (
+                      <div className="options">
+                        <span className="label">Buy Print:</span>
+                        <span 
+                          className="purchase buy-print" 
+                          onClick={() => console.log('Buy Poster #' + currentImage.posterId)}
+                        >Poster</span>
+                      </div>
+                    )}
+                    
+                  </div>
+                  
+                </div>
               )}
               
             </main>

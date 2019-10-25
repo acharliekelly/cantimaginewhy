3da456f0056352d4b2f4a922e58e50cb1c3844a7
Charlie Kelly <charliekelly31415@gmail.com>
2019-10-24 22:58:43 -0400

    adjusted site title position

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ImageCarousel from './ImageCarousel';
-
+import '../css/Home.scss';
 
 export default function Home() {
   
    return (
       <div className="content">
+        <div className="site-title">Can't Imagine Why</div>
         <main>
-          <header className="site-title">Can't Imagine Why</header>
+          
           <ImageCarousel tagName="ck" />
         </main>
       </div>

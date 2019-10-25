c533490f69c0b1dc484bdbb418cd4d77671a9643
Charlie Kelly <charliekelly31415@gmail.com>
2019-10-24 22:38:56 -0400

    change carousel gallery

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ImageCarousel from './ImageCarousel';
 
-const clickImage = img => {
-  console.log('image clicked: ', img)
-} 
 
 export default function Home() {
   
    return (
       <div className="content">
         <main>
-          <header>Can't Imagine Why</header>
-          <ImageCarousel tagName="favorite" selectImage={clickImage} />
+          <header className="site-title">Can't Imagine Why</header>
+          <ImageCarousel tagName="ck" />
         </main>
       </div>
     );

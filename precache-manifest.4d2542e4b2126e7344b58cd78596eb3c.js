e456edcbabcdb3781d1a6680258febf85f482347
Charlie Kelly <charliekelly31415@gmail.com>
2019-10-27 21:05:35 -0400

    Updates

---
+++ precache-manifest.4d2542e4b2126e7344b58cd78596eb3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e4bbf44cb08ab5cd464b3b810c75cd",
+    "url": "/cantimaginewhy/index.html"
+  },
+  {
+    "revision": "6d2adc3b811b61fe73eb",
+    "url": "/cantimaginewhy/static/css/2.960f6ed1.chunk.css"
+  },
+  {
+    "revision": "4cb41e7d9ffe5650cdf5",
+    "url": "/cantimaginewhy/static/css/main.98ab7aeb.chunk.css"
+  },
+  {
+    "revision": "6d2adc3b811b61fe73eb",
+    "url": "/cantimaginewhy/static/js/2.a0fbd719.chunk.js"
+  },
+  {
+    "revision": "4cb41e7d9ffe5650cdf5",
+    "url": "/cantimaginewhy/static/js/main.33d543a3.chunk.js"
+  },
+  {
+    "revision": "4e2a46becae476a3ca10",
+    "url": "/cantimaginewhy/static/js/runtime~main.ded85c8a.js"
+  }
+]);

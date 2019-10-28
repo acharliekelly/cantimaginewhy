883f19494a1c86adffbbc618cd2e0be25091443a
Charlie Kelly <charliekelly31415@gmail.com>
2019-10-27 21:02:48 -0400

    social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Image } from 'cloudinary-react';
+import { Image, CloudinaryContext } from 'cloudinary-react';
 
 const Footer = () => (
   <div className="footer">
     <span className="copyright">&copy;2019 by Charlie Kelly</span>
     <span className="social">
-      <Image cloudName="cantimaginewhy" publicId="instagram_logo" height="100" />
-      <a target="_blank" rel=" noopener noreferrer" href="https://instagram.com/cant_imagine_why">
-        @cant_imagine_why
-      </a>
+      <CloudinaryContext cloudName="cantimaginewhy">
+        <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cant_imagine_why">
+          <Image publicId="instagram_logo" height="100" />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://facebook.com/acharliekelly">
+          <Image publicId="facebook_logo" height="100" />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/acharliekelly">
+          <Image publicId="github-logo" height="100" />
+        </a>
+      </CloudinaryContext>
     </span>
   </div>
 );

aa9d1b4aae0b887d81844219b65d5e38b10023b2
Charlie Kelly <charliekelly31415@gmail.com>
2019-10-27 20:38:31 -0400

    change home carousel back to favorites

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="site-title">Can't Imagine Why</div>
         <main>
           
-          <ImageCarousel tagName="ck" />
+          <ImageCarousel tagName="favorite" />
         </main>
       </div>
     );

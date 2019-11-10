47339b1034ee44fc5c3d2941ae7581541a425c16
Charlie Kelly <charliekelly31415@gmail.com>
2019-11-10 14:29:37 -0500

    reformat menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,30 +8,43 @@ const Menu = (handleLogin, handleSearch) => {
   return (
     <CloudinaryContext cloudName="cantimaginewhy">
       <header className="menu">
-        <div className="column left-col justify-content-center site-logo lg-2">
+        <div className="site-logo">
           <Image publicId="ck_logo" height="100" />
         </div>
-        <Nav className="column mid-col lg-4" defaultActiveKey="/home">
-          <Nav.Item>
-            <NavLink to="/home">Home</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink to="/artwork">Artwork</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink to="/about">About</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink to="/contact">Contact</NavLink>
-          </Nav.Item>
-        </Nav>
-        <div className="column right-col lg-2">
-          <div className="top-row">
-            <Image className="login" publicId="icon/login-icon" width="30" />
-            <span className="login-text">Sign Up / Login</span>
-            <Image className="cart" publicId="icon/shopping-cart" width="30" />
+        <div className="nav-grid">
+          <div className="top-row left-col">
+            <div className="wip">This site is a Work In Progress</div>
           </div>
-          <div className="bottom-row">
+          <div className="top-row right-col">
+            <Image  
+              className="login" 
+              publicId="icon/login-icon" 
+              width="30" 
+              onClick={handleLogin}
+            />
+            <span className="login-text">Sign Up / Login</span>
+            <Image  
+              className="cart" 
+              publicId="icon/shopping-cart" 
+              width="30" 
+              onClick={handleSearch}
+            />
+          </div>
+          <Nav className="bottom-row left-col" defaultActiveKey="/home">
+            <Nav.Item>
+              <NavLink to="/home">Home</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink to="/artwork">Artwork</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink to="/about">About</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink to="/contact">Contact</NavLink>
+            </Nav.Item>
+          </Nav>
+          <div className="bottom-row right-col">
             <Form.Control type="text" size="sm" placeholder="e.g. Boston, Summer, etc" />
           </div>
         </div>

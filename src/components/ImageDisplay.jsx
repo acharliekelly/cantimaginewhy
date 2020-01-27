4caf1d08d4b9d944b9317c8773b15a444cfbb5fc
Charlie Kelly <charliekelly31415@gmail.com>
2020-01-27 11:41:48 -0500

    add hooks

--- src/components/ImageDisplay.jsx
+++ src/components/ImageDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Lightbox from 'react-image-lightbox';
 import { variableImageSrc, watermarkedImageSrc } from '../utils/imageApi';
@@ -6,38 +6,32 @@ import { orderEmailLink } from '../utils/contactApi';
 import 'react-image-lightbox/style.css';
 import '../css/display.scss';
 
-class ImageDisplay extends Component {
-  
-  constructor (props) {
-    super(props);
-    this.state = {
-      lightboxOpen: false
-    }
-  }
+const ImageDisplay = props => {
+  const [ lightboxOpen, setLightboxOpen ] = useState(false);
 
-  loadImageProperties = () => {
+  const loadImageProperties = () => {
     const infoObj = {
-      id: this.props.currentImage.public_id,
-      source: variableImageSrc(this.props.currentImage.public_id, 600),
-      title: this.getPictureCaption(),
-      description: this.getPictureProperty('alt'),
-      location: this.getPictureProperty('location'),
-      medium: this.getPictureProperty('medium'),
-      size: this.getPictureProperty('size'),
-      year: this.getPictureProperty('year'),
-      forSale: (this.getPictureProperty('original') === 'available'),
-      price: this.getPictureProperty('price', 'NFS'),
-      materialInfo: this.hasProperty('medium') && this.hasProperty('size'),
+      id: props.currentImage.public_id,
+      source: variableImageSrc(props.currentImage.public_id, 600),
+      title: getPictureCaption(),
+      description: getPictureProperty('alt'),
+      location: getPictureProperty('location'),
+      medium: getPictureProperty('medium'),
+      size: getPictureProperty('size'),
+      year: getPictureProperty('year'),
+      forSale: (getPictureProperty('original') === 'available'),
+      price: getPictureProperty('price', 'NFS'),
+      materialInfo: hasProperty('medium') && hasProperty('size'),
     }
     return infoObj;
   }
 
-  getPictureCaption = () => {
-    return this.getPictureProperty('caption', 'Untitled');
+  const getPictureCaption = () => {
+    return getPictureProperty('caption', 'Untitled');
   }
 
-  getPictureProperty = (property, errValue = '') => {
-    const pictureObj = this.props.currentImage;
+  const getPictureProperty = (property, errValue = '') => {
+    const pictureObj = props.currentImage;
     let val;
     try {
       val = pictureObj.context.custom[property];
@@ -47,8 +41,8 @@ class ImageDisplay extends Component {
     return val;
   }
 
-  hasProperty = propertyName => {
-    const pictureObj = this.props.currentImage;
+  const hasProperty = propertyName => {
+    const pictureObj = props.currentImage;
     if (pictureObj.hasOwnProperty('context')) {
       return pictureObj.context.custom.hasOwnProperty(propertyName);
     } else {
@@ -56,73 +50,64 @@ class ImageDisplay extends Component {
     }
   }
 
-  handlePurchase = () => {
-    const imgId = this.props.currentImage.public_id;
-    this.props.purchaseItem(imgId);
+  // const handlePurchase = () => {
+  //   const imgId = props.currentImage.public_id;
+  //   props.purchaseItem(imgId);
+  // }
+
+  const openLightbox = () => {
+    setLightboxOpen(true);
   }
 
-  openLightbox = () => {
-    this.setState({
-      lightboxOpen: true
-    })
+  const closeLightbox = () => {
+    setLightboxOpen(false);
   }
 
-  closeLightbox = () => {
-    this.setState({
-      lightboxOpen: false
-    })
-  }
-
-  render () {
-    const { currentImage } = this.props;
-    if (currentImage) {
-      const info = this.loadImageProperties();
-      return (
-        <div className="image-view">
-          <img className="display-image" alt="" src={info.source} onClick={this.openLightbox} />
-          <div className="image-info">
-            <div className="title">{info.title}</div>
-            <div className="info">{info.description}</div>
-            {info.location && (
-            <div className="info">
-              <span className="label">Location: </span>
-              <span className="data">{info.location}</span>
-            </div>
-            )}
-            {info.year && (
-            <div className="info">
-              <span className="label">Year: </span>
-              <span className="data">{info.year}</span>
-            </div>
-            )}
-            {info.materialInfo && (
-            <div className="info">
-              <span className="label">Material: </span>
-              <span className="data">{info.size}, {info.medium}</span>
-            </div>
-            )}
-            {info.forSale && (
-              <div className="options">
-                <span className="label">Buy Original:</span>
-                <a className="purchase buy-orig" href={orderEmailLink(info.id)}>${info.price}</a>
-              </div>
-            )}
+  if (props.currentImage) {
+    const info = loadImageProperties();
+    return (
+      <div className="image-view">
+        <img className="display-image" alt="" src={info.source} onClick={openLightbox} />
+        <div className="image-info">
+          <div className="title">{info.title}</div>
+          <div className="info">{info.description}</div>
+          {info.location && (
+          <div className="info">
+            <span className="label">Location: </span>
+            <span className="data">{info.location}</span>
           </div>
-          
-          {this.state.lightboxOpen && (
-            <Lightbox 
-              mainSrc={watermarkedImageSrc(currentImage.public_id)}
-              onCloseRequest={this.closeLightbox}
-            />
+          )}
+          {info.year && (
+          <div className="info">
+            <span className="label">Year: </span>
+            <span className="data">{info.year}</span>
+          </div>
+          )}
+          {info.materialInfo && (
+          <div className="info">
+            <span className="label">Material: </span>
+            <span className="data">{info.size}, {info.medium}</span>
+          </div>
+          )}
+          {info.forSale && (
+            <div className="options">
+              <span className="label">Buy Original:</span>
+              <a className="purchase buy-orig" href={orderEmailLink(info.id)}>${info.price}</a>
+            </div>
           )}
         </div>
+        
+        {lightboxOpen && (
+          <Lightbox 
+            mainSrc={watermarkedImageSrc(props.currentImage.public_id)}
+            onCloseRequest={closeLightbox}
+          />
+        )}
+      </div>
 
-      );
-    } else {
-      return (<div />)
-    }
-    
-    
+    );
+  } else {
+    return (<div />)
   }
   
 }

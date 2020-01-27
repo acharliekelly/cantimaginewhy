d68c402c3c3b3c8cfdeeef80d3512ec2fd3f65d0
Charlie Kelly <charliekelly31415@gmail.com>
2020-01-27 11:40:56 -0500

    move social media links to top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { Image, CloudinaryContext } from 'cloudinary-react';
+// import { Image, CloudinaryContext } from 'cloudinary-react';
 
 const Footer = () => (
   <div className="footer">
     <span className="copyright">&copy;2019 by Charlie Kelly</span>
-    <span className="social">
-      <CloudinaryContext cloudName="cantimaginewhy">
-        <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/cant_imagine_why">
-          <Image publicId="icon/instagram_logo" height="100" />
-        </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://facebook.com/acharliekelly">
-          <Image publicId="icon/facebook_logo" height="100" />
-        </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/acharliekelly">
-          <Image publicId="icon/github_logo" height="100" />
-        </a>
-      </CloudinaryContext>
-    </span>
+    
   </div>
 );
 

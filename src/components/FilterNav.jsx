e898bb49b0456ac068b5049dfd9d3cdf4ca53900
Charlie Kelly <charliekelly31415@gmail.com>
2020-01-27 11:36:56 -0500

    refactor

--- src/components/FilterNav.jsx
+++ src/components/FilterNav.jsx
@@ -1,99 +1,83 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Nav from 'react-bootstrap/Nav';
 import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
 import { filters } from '../utils/filters';
 import '../css/galleryNav.scss';
 
-class FilterNav extends Component {
+const FilterNav = props => {
+  // const [filterIndex, setFilterIndex] = useState(0);
+  const [selectedNav, setSelectedNav] = useState(null);
+  const [selectedFilterIndex, setSelectedFilterIndex] = useState(0);
 
-  constructor (props) {
-    super(props);
+  const handleFilterSelect = eventKey => {
+    setSelectedFilterIndex(eventKey);
+    setSelectedNav(null);
+    props.handleClearGallery();
+  };
 
-    this.state = {
-      filterIndex: 0,
-      selectedNav: null
-    }
+  const handleNavClick = nav => {
+    setSelectedNav(nav);
+    props.handleNavChange(nav.tag);
   }
 
-  componentDidMount () {
-    this.handleFilterSelect(0);
+  if (props.filterIndex) {
+    setSelectedFilterIndex(props.filterIndex);
   }
-
-  handleFilterSelect = eventKey => {
-    // console.log('Sort selected: ' + eventKey);
-    this.setState({
-      filterIndex: eventKey,
-      selectedNav: null
-    });
-    // clear thumbnails
-    this.props.handleClearGallery();
-  }
-
-  handleNavClick = nav => {
-    // nav is object (option) from filter list
-    // { name, tag, thumbnail, description }
-    this.setState({
-      selectedNav: nav
-    });
-
-    this.props.handleNavChange(nav.tag);
-  }
-
-  render () {
-    const { filterIndex, selectedNav } = this.state;
-    const activeFilter = filters[filterIndex];
-    return (
-      <CloudinaryContext cloudName="cantimaginewhy">
-        <Nav 
-          variant="pills" 
-          defaultActiveKey={0} 
-          className="gallery-nav justify-content-center" 
-          onSelect={this.handleFilterSelect}
-        >
-          <span className="label">Filter By:</span>
-          {filters.map(filter => (
-            <Nav.Item key={filter.name}>
-              <Nav.Link eventKey={filter.index}>{filter.name}</Nav.Link>
-            </Nav.Item>
-            )
-          )}
-        </Nav>
-        <div className="album-list">
-          {activeFilter.options.map(option => {
-              let cls = 'album-btn responsive thumbnail';
-              if (selectedNav && selectedNav.tag === option.tag) {
-                cls += ' selected-nav'
-              }
-              return (
-                <div key={option.tag} id={option.tag} className={cls} onClick={() => this.handleNavClick(option)}>
-                    <Image  
-                        publicId={`${option.thumbnail}`}
-                        height="100"
-                        crop="fit"
-                        quality="80"
-                    >
-                        <Transformation quality="auto" fetchFormat="auto" />
-                    </Image>
-                    <div className="album-name">{option.name}</div>
-                </div>
-              );
-            })}
-          </div>
-        {selectedNav && (
-          <div className="current-nav">
-            <div className="nav-title">{selectedNav.name}</div>
-            <div className="nav-description">{selectedNav.description}</div>
-          </div>
+  
+  return (
+    <CloudinaryContext cloudName="cantimaginewhy">
+      {/* Filter types */}
+      <Nav 
+        variant="pills" 
+        defaultActiveKey={0} 
+        className="gallery-nav justify-content-center" 
+        onSelect={handleFilterSelect}
+      >
+        <span className="label">Filter By:</span>
+        {filters.map(filter => (
+          <Nav.Item key={filter.name}>
+            <Nav.Link eventKey={filter.index}>{filter.name}</Nav.Link>
+          </Nav.Item>
+          )
         )}
-      </CloudinaryContext>
-    );
-  }
+      </Nav>
+      {/* actual filters, with thumbnails  */}
+      <div className="album-list">
+        {filters[selectedFilterIndex].options.map(option => {
+            let cls = 'album-btn responsive thumbnail';
+            if (selectedNav && selectedNav.tag === option.tag) {
+              cls += ' selected-nav'
+            }
+            return (
+              <div key={option.tag} id={option.tag} className={cls} onClick={() => handleNavClick(option)}>
+                  <Image  
+                      publicId={`${option.thumbnail}`}
+                      height="60"
+                      crop="fit"
+                      quality="80"
+                  >
+                      <Transformation quality="auto" fetchFormat="auto" />
+                  </Image>
+                  <div className="album-name">{option.name}</div>
+              </div>
+            );
+          })}
+        </div>
+      {selectedNav && (
+        <div className="current-nav">
+          <div className="nav-title">{selectedNav.name}</div>
+          <div className="nav-description">{selectedNav.description}</div>
+        </div>
+      )}
+    </CloudinaryContext>
+  );
 }
 
 FilterNav.propTypes = {
   handleNavChange: PropTypes.func.isRequired,
-  handleClearGallery: PropTypes.func
+  handleClearGallery: PropTypes.func,
+  filterIndex: PropTypes.number
 };
 
 export default FilterNav;

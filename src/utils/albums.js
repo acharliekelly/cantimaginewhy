d7360c72c4d74af44e5ec963dd7ad9edf0fbc845
Charlie Kelly <charliekelly31415@gmail.com>
2020-01-27 14:53:30 -0500

    add album list

---
+++ src/utils/albums.js
@@ -0,0 +1,26 @@
+export const albums = [
+  {
+    "name": "Plein Air",
+    "tag": "en plein air",
+    "thumbnail": "art/memorial_drive_z68y3q",
+    "description": "Painted on location"
+  },
+  {
+    "name": "Scenery",
+    "tag": "scenery",
+    "thumbnail": "art/esplanade-sunset_2018",
+    "description": "Landscapes and such"
+  },
+  {
+    "name": "Knotwork",
+    "tag": "knotwork",
+    "thumbnail": "art/lotus_26804737776_o_i7e8dw",
+    "description": "Abstract art inspired by Celtic tradition"
+  },
+  {
+    "name": "Holidays",
+    "tag": "holiday",
+    "thumbnail": "art/valentines-card-2012",
+    "description": "Holiday cards"
+  }
+]

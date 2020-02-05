9d65768d6c691b792df93f3ef37d521ddca595d3
Charlie Kelly <charliekelly31415@gmail.com>
2020-02-05 17:32:02 -0500

    add process photo calls

--- src/utils/imageApi.js
+++ src/utils/imageApi.js
@@ -23,16 +23,6 @@ export const fetchGallery = tagName => {
   return axios(url);
 }
 
-// Return JSON data of all images where context.ref = keyName
-export const fetchRelatedImages = (keyName, callback) => {
-  cloudinary.v2.search
-    .expression(`context.ref:${keyName}`)
-    .with_field('context')
-    .with_field('tags')
-    .max_results(10)
-    .execute().then(results => callback(results));
-}
-
 // Image URLs
 
 
@@ -74,6 +64,58 @@ export const paddedImageSrc = (publicId, width = 600, height = 400) => {
   return imgSrc + `w_${width},h_${height},c_pad,b_white/${publicId}.jpg`;
 }
 
+
+/*  SEARCH FUNCTIONS
+ *
+ * (Promisified)
+ */
+
+const fetchImages = (keyName, callback, tag = '') => {
+  let expr = `context.ref:${keyName} AND folder=photos`;
+  if (tag !== '') {
+    expr += ` AND tags=${tag}`;
+  }
+  cloudinary.v2.search
+    .expression(expr)
+    .with_field('context')
+    .with_field('tags')
+    .max_results(10)
+    .execute().then(results => callback(results));
+}
+
+export const axiosFetchImages = (keyName, callback) => {
+  // TODO: copy endpoint url from Cloudinary docs
+  const url = `https://${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/${cloudName}/resources/search`;
+  axios.get(url, {
+    params: {
+      expression: `context.ref:${keyName} AND folder=photos`,
+      with_field: 'context',
+      max_results: 20
+    }
+  }).then(function(response) {
+    callback(response)
+  }).catch(function(err) {
+    console.error(err)
+  })
+}
+
+// Return JSON data of all images where context.ref = keyName
+export const fetchRelatedImages = (keyName, callback) => {
+  fetchImages(keyName, callback);
+}
+
+// Only return Progress images
+export const fetchProgressImages = (keyName, callback) => {
+  fetchImages(keyName, callback, 'progress');
+}
+
+export const fetchViewImages = (keyName, callback) => {
+  fetchImages(keyName, callback, 'view');
+}
+
+export const fetchFinalImage = (keyName, callback) => {
+  fetchImages(keyName, callback, 'final');
+}
 
 
 // perform text search - returns Promise

f03ed21394392631a8b775d6f3cd76150dceefd7
Charlie Kelly <charliekelly31415@gmail.com>
2020-02-17 15:19:30 -0500

    split Contact and Order forms

--- src/components/ImageDisplay/index.jsx
+++ src/components/ImageDisplay/index.jsx
@@ -3,9 +3,8 @@ import PropTypes from 'prop-types';
 import Lightbox from 'react-image-lightbox';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { paddedImageSrc, lightboxImageSrc, axiosFetchImages } from '../../utils/imageApi';
-// import { loadImageProps } from '../../utils/imageContext';
 import  { faaAvailable, faaLookup } from '../../utils/fineArtApi';
-import { OrderForm } from '../Contact';
+import { OrderForm } from '../Order/';
 import 'react-image-lightbox/style.css';
 import './display.scss';
 

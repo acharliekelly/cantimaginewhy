02ff291931a53fba79ba7cb66b2b1ebdedab987f
Charlie Kelly <charliekelly31415@gmail.com>
2020-02-25 21:51:21 -0500

    add Jest

---
+++ src/utils/finArtApi.test.js
@@ -0,0 +1,11 @@
+const { faaAvailable, faaLookup } = require('./fineArtApi');
+
+
+test('check if image exists in dictionary', () => {
+  const isAvailable = faaAvailable('art/a_friday_in_september');
+  expect(isAvailable).toBeTruthy();
+});
+
+test('check if lookup returns correct code', () => {
+  expect(faaLookup('art/boston-public-gardens_2019')).toBe('boston-public-garden-1');
+});

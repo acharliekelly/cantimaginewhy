fa9295c7f09e4dc36d56b6a98491d006843f0ed5
Charlie Kelly <charliekelly31415@gmail.com>
2020-02-27 03:01:00 -0500

    fix ProgressGallery

--- src/components/ProgressGallery/ProgressGallery.stories.js
+++ src/components/ProgressGallery/ProgressGallery.stories.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { Image } from 'cloudinary-react';
 import ProgressGallery from './';
 
-import '../../css/main.scss';
+import './process.scss';
 
-const mockRef = 'public_garden_2';
+const mockRef = 'memorial_drive';
+
+const selectLightbox = imageId => {
+  console.log('open lightbox for image: ' + imageId);
+}
 
 
 const stories = storiesOf('ProgressGallery', module);
 
 stories.add('Progress Gallery', () => {
   return (
-    <ProgressGallery refKey={mockRef} />
+    <div className="story">
+      <Image 
+        className="display-image"
+        cloudName="cantimaginewhy"
+        publicId="art/memorial_drive"
+        height="400"
+      />
+    
+      <ProgressGallery refKey={mockRef} selectLightbox={selectLightbox} imageHeight={60} />
+    </div>
   )
 })
--- src/components/ProgressGallery/index.jsx
+++ src/components/ProgressGallery/index.jsx
@@ -1,99 +1,85 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Image } from 'cloudinary-react';
-import { onsiteViewImages, onsiteProgressImages, onsiteFinalImage } from '../../utils/processImages';
+import { CloudinaryContext, Image, Transformation } from 'cloudinary-react';
+// import { 
+//   onsiteViewImages, 
+//   onsiteProgressImages, 
+//   onsiteFinalImage, 
+//   ImageProcessTypes,
+//   getRef
+// } from '../../utils/processImages';
+import { getSeries } from '../../utils/processUtils';
+
+
+import './process.scss';
 
 class ProgressGallery extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      viewImages: [],
-      progressImages: [],
-      finalImage: '',
-      isLoaded: false
+      progressImages: []
     }
   }
 
-  componentDidUpdate () {
+  componentDidMount () {
     const { refKey } = this.props;
-    console.log('refKey property: ' + refKey);
-    if (!this.state.isLoaded) {
-      this.updateImages(refKey);
-      console.log('View: ' + this.state.viewImages)
-      console.log('Progress: ' + this.state.progressImages)
-      console.log('Final: ' + this.state.finalImage);
-    }
+    this.updateImages(refKey);
+    
   }
+
 
   updateImages = refKey => {
     console.log('updating process images for #' + refKey);
-    this.setState({
-      viewImages: onsiteViewImages(refKey),
-      progressImages: onsiteProgressImages(refKey),
-      finalImage: onsiteFinalImage(refKey),
-      isLoaded: true
-    })
+    const series = getSeries(refKey);
+    if (series) {
+      this.setState({
+        progressImages: series
+      })
+    } else {
+      console.log('- no images found')
+    }
+    
   }
+
 
   handleImageClick = ev => {
     // TODO: expand images
   }
 
   render () {
-    const { viewImages, progressImages, finalImage, isLoaded } = this.state;
-    if (!isLoaded) {
-      return <h1 className="loading">Loading</h1>
-    } else {
-      return (
-        <div className="process-images">
-          <div className="view">
-            <label>View Images: <strong>{viewImages.length}</strong></label>
-            <span className="view-imgs">
-            {viewImages.map(imageId => (
-              <Image 
-                className="responsive thumbnail" 
-                cloudName="cantimaginewhy" 
-                publicId={imageId} 
-                height="80"
-              />
-            ))}
-            </span>
-          </div>
-          <div className="progress">
-            <label>Progress Images: <strong>{progressImages.length}</strong></label>
-            <span className="progress-imgs">
-              {progressImages.map(imageId => (
-                <Image 
-                  cloudName="cantimaginewhy" 
-                  className="responsive thumbnail"
-                  publicId={imageId}
-                  height="80"
-                />
-              ))}
-            </span>
-          </div>
-          <div className="final">
-            <label>Final Image: <strong>{finalImage ? 1 : 0}</strong></label>
-            <span className="final-img">
-              {finalImage && (
-                <Image 
-                  cloudName="cantimaginewhy" 
-                  className="responsive thumbnail"
-                  publicId={finalImage}
-                  height="80"
-                />
-              )}
-            </span>
-          </div>
-        </div>
-      )
-    }
+    const { progressImages } = this.state;
+    const { selectLightbox, imageHeight } = this.props;
+    return (
+      <CloudinaryContext cloudName="cantimaginewhy">
+      <div className="process-images">
+          {progressImages.map(imageId => (
+            <Image 
+              key={imageId}
+              title={imageId}
+              className="responsive thumbnail" 
+              publicId={imageId} 
+              height={imageHeight || 80}
+              onClick={() => { 
+                if (selectLightbox) {
+                  selectLightbox(imageId);
+                } else {
+                  console.log(imageId + ' clicked');
+                }
+              }}
+            >
+              <Transformation defaultImage="ck-diamond.jpg" />
+            </Image>
+          ))}     
+      </div>
+      </CloudinaryContext>
+    )
   }
 }
 
 ProgressGallery.propTypes = {
   refKey: PropTypes.string.isRequired,
-  openLightbox: PropTypes.func
+  imageHeight: PropTypes.number,
+  selectLightbox: PropTypes.func
 }
 
 export default ProgressGallery;

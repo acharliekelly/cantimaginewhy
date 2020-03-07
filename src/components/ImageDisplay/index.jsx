c97f7139f05fa7002e715a72674bae8a90e75d8e
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-06 23:58:42 -0500

    show progress imgs over display

--- src/components/ImageDisplay/index.jsx
+++ src/components/ImageDisplay/index.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { paddedImageSrc, defaultCPI } from '../../utils/imageApi';
+import { CloudinaryContext, Image } from 'cloudinary-react';
 import { selectLightboxUtil, moveNextUtil, movePreviousUtil } from '../../utils/imageUtils';
 import  { faaAvailable, faaLookup } from '../../utils/fineArtApi';
 import { isSeriesExist } from '../../utils/onsiteUtils';
+import { lookupSeriesCount } from '../../utils/processUtils';
 import { OrderForm } from '../OrderForm/';
 import ProgressGallery from '../ProgressGallery/';
 
 import './display.scss';
+
+// TODO: break this up into smaller components!
 
 class ImageDisplay extends Component {
   
@@ -16,15 +19,15 @@ class ImageDisplay extends Component {
     super(props);
     this.state = {
       orderFormOpen: false,
-      processOpen: false
+      processImageId: null
     }
   }
 
   loadImageProperties = () => {
     const imgId = this.props.currentImage.public_id;
+    const ref = this.getPictureProperty('key', '-');
     const infoObj = {
       id: imgId,
-      source: paddedImageSrc(imgId, 600, 400),
       title: this.getPictureCaption(),
       description: this.getPictureProperty('alt'),
       location: this.getPictureProperty('location'),
@@ -33,8 +36,8 @@ class ImageDisplay extends Component {
       year: this.getPictureProperty('year'),
       forSale: (this.getPictureProperty('original') === 'available'),
       forPrint: (faaAvailable(imgId)),
-      refKey: this.getPictureProperty('key', '-'),
-      processImgs: isSeriesExist(imgId),
+      refKey: ref,
+      processImgs: isSeriesExist(ref),
       price: this.getPictureProperty('price', 'NFS'),
       materialInfo: this.hasProperty('medium') && this.hasProperty('size'),
     }
@@ -69,17 +72,46 @@ class ImageDisplay extends Component {
     ev.preventDefault();
   }
 
-
-  handleProcess = ev => {
-    
-    const refKey = this.getPictureProperty('key', '');
-    console.log('Open images for: ' + refKey);
-    ev.preventDefault();
-  }
-
   openLightbox = () => {
     const { currentImage, selectLightbox } = this.props;
     selectLightbox(currentImage.public_id);
+  }
+
+  setProcessImage = image => {
+    // console.log('setting process image: ' + image)
+    this.setState({
+      processImageId: image
+    })
+  }
+
+  closeProcess = ev => {
+    this.setState({
+      processImageId: null
+    })
+    ev.preventDefault()
+  }
+
+  nextProcessImage = ev => {
+    // TODO: move to next image via ProgressGallery
+    const { processImageId } = this.state;
+    const refKey = processImageId.split('-')[1];
+    const imgOrder = processImageId.split('-')[2];
+    
+    let strNext = '0';
+    if (imgOrder.length === 1) { // imgOrder is a number
+      const nMax = lookupSeriesCount(refKey);
+      const nNext = parseInt(imgOrder) + 1;
+      if (nNext <= nMax) {
+        strNext = nNext.toString();
+      } else {
+        strNext = 'final';
+      }
+    } // else imgOrder is 'final', so next should be '0'
+    
+    const nextImageId = `photos/onsite-${refKey}-${strNext}`;
+    this.setProcessImage(nextImageId);
+
+    ev.stopPropagation(); // prevent parent from closing image
   }
 
 
@@ -107,134 +139,138 @@ class ImageDisplay extends Component {
     })
   }
 
-  showProcessImages = () => {
-    this.setState({ processOpen: true })
-  }
-
-  hideProcessImages = () => {
-    this.setState({ processOpen: false })
-  }
-
-  toggleProcessImages = e => {
-    const { processOpen } = this.state;
-    this.setState({
-      processOpen: !processOpen,
-      orderFormOpen: false
-    })
-    e.preventDefault();
-  }
-
   componentDidUpdate (prevProps, prevState) {
     if (prevProps.currentImage !== this.props.currentImage) {
       this.setState({
-        processOpen: false,
-        orderFormOpen: false
+        orderFormOpen: false,
+        processImageId: null
       })
     }
   }
 
   render () {
-    const { currentImage, selectLightbox } = this.props;
-    const { orderFormOpen, processOpen } = this.state;
+    const { currentImage } = this.props;
+    const { orderFormOpen, processImageId } = this.state;
     if (currentImage) {
       const info = this.loadImageProperties();
       return (
-        <div className="image-box">
-          <div className="image-nav-btn prev-btn" onClick={this.props.movePrevious}>
-            <FontAwesomeIcon icon="chevron-circle-left" size="lg" />
-          </div>
-          <div className="image-view">
-            <img className="display-image" alt="" src={info.source} onClick={this.openLightbox} />
-          </div>
-          <div className="image-nav-btn next-btn" onClick={this.props.moveNext}>
-            <FontAwesomeIcon icon="chevron-circle-right" size="lg" />
-          </div>
-
-          <div className="spacer" />
-          <div className="image-info">
-            <div className="title">{info.title}</div>
-            <div className="descript">{info.description}</div>
-            {info.location && (
-            <div className="info">
-              <span className="label">Location: </span>
-              <span className="data">{info.location}</span>
+        <CloudinaryContext cloudName="cantimaginewhy">
+          <div className="image-box">
+            <div className="image-nav-btn prev-btn" onClick={this.props.movePrevious}>
+              <FontAwesomeIcon icon="chevron-circle-left" size="lg" />
             </div>
-            )}
-            {info.year && (
-            <div className="info">
-              <span className="label">Year: </span>
-              <span className="data">{info.year}</span>
-            </div>
-            )}
-            {info.materialInfo && (
-            <div className="info">
-              <span className="label">Material: </span>
-              <span className="data">{info.size}, {info.medium}</span>
-            </div>
-            )}
-
-            
-            <div className="options">
-              <span className="label">Original:</span>
-              {info.forSale && (
-                <a 
-                  className="feature buy-orig" 
-                  href="/" 
-                  onClick={this.showOrderForm}
-                  >
-                    ${info.price}
-                </a>
-              )}
-              {!info.forSale && (
-                <a 
-                  className="feature nfs" 
-                  href="/" 
-                  onClick={this.suppressLink}
+            <div className="image-view">
+              <Image 
+                className="display-image"
+                publicId={info.id}
+                width="600"
+                height="400"
+                crop="pad"
+                onClick={this.openLightbox}
+              />
+              {processImageId && (
+                <div 
+                  className="process-image"
+                  onClick={this.closeProcess}
                 >
-                  Sold
-                </a>
+                  <Image 
+                    publicId={processImageId}
+                    onClick={this.nextProcessImage}
+                    width="400"
+                    height="250"
+                    crop="pad"
+                    background="black"
+                  />
+
+                </div>
+
               )}
             </div>
-            {info.forPrint && (
-              <div className="options">
-                <span className="label">Prints: </span>
-                <a 
-                  className="feature buy-print" 
-                  rel="noopener noreferrer" 
-                  target="_blank" 
-                  href={faaLookup(info.id)}
-                  >Available</a>
+            <div className="image-nav-btn next-btn" onClick={this.props.moveNext}>
+              <FontAwesomeIcon icon="chevron-circle-right" size="lg" />
+            </div>
+
+            <div className="spacer" />
+            <div className="image-info">
+              <div className="title">{info.title}</div>
+              <div className="descript">{info.description}</div>
+              {info.location && (
+              <div className="info">
+                <span className="label">Location: </span>
+                <span className="data">{info.location}</span>
               </div>
-            )}
-            { info.processImgs && (
+              )}
+              {info.year && (
+              <div className="info">
+                <span className="label">Year: </span>
+                <span className="data">{info.year}</span>
+              </div>
+              )}
+              {info.materialInfo && (
+              <div className="info">
+                <span className="label">Material: </span>
+                <span className="data">{info.size}, {info.medium}</span>
+              </div>
+              )}
+
+              
               <div className="options">
-                <a href="/" className="feature view-process" onClick={this.toggleProcessImages}>Process</a>
-                {processOpen && (
-                    <ProgressGallery 
-                      refKey={info.refKey} 
-                      imageHeight={60}
-                      selectLightbox={selectLightbox} 
-                    />
+                <span className="label">Original:</span>
+                {info.forSale && (
+                  <a 
+                    className="feature buy-orig" 
+                    href="/" 
+                    onClick={this.showOrderForm}
+                    >
+                      ${info.price}
+                  </a>
                 )}
-                
+                {!info.forSale && (
+                  <a 
+                    className="feature nfs" 
+                    href="/" 
+                    onClick={this.suppressLink}
+                  >
+                    Sold
+                  </a>
+                )}
               </div>
-            )}
-            {(orderFormOpen && info.forSale) && (
-              <div className="form">
-                <a className="close-btn" href="/" onClick={this.hideOrderForm}>X</a>
-                <OrderForm 
-                  className="inner-form"
-                  imageId={info.id}
-                  price={info.price}
-                  closeForm={this.hideOrderForm}
-                />
-              </div>
-            )}
+              {info.forPrint && (
+                <div className="options">
+                  <span className="label">Derived Products: </span>
+                  <a 
+                    className="feature buy-print" 
+                    rel="noopener noreferrer" 
+                    target="_blank" 
+                    href={faaLookup(info.id)}
+                    >Available</a>
+                </div>
+              )}
+              { info.processImgs && (
+                <div className="options">
+                  <ProgressGallery 
+                    refKey={info.refKey} 
+                    imageHeight={60}
+                    selectImage={this.setProcessImage} 
+                  />
+                </div>
+              )}
+              {(orderFormOpen && info.forSale) && (
+                <div className="form">
+                  <a className="close-btn" href="/" onClick={this.hideOrderForm}>X</a>
+                  <OrderForm 
+                    className="inner-form"
+                    imageId={info.id}
+                    price={info.price}
+                    closeForm={this.hideOrderForm}
+                  />
+                </div>
+              )}
+            </div>
+            <div className="spacer" />
+
           </div>
-          <div className="spacer" />
-
-        </div>
-
+        </CloudinaryContext>
       );
     } else {
       return (<div />)
@@ -265,7 +301,7 @@ ImageDisplay.propTypes = {
 }
 
 ImageDisplay.defaultProps = {
-  currentImage: defaultCPI,
+  currentImage: null,
   selectLightbox: selectLightboxUtil,
   moveNext: moveNextUtil,
   movePrevious: movePreviousUtil
--- src/components/ProgressGallery/index.jsx
+++ src/components/ProgressGallery/index.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CloudinaryContext, Image, Transformation } from 'cloudinary-react';
 import { onsitePhotos } from '../../utils/onsiteUtils';
-// import { fetchProcessImages } from '../../utils/advImageApi';
 import { defaultCPI } from '../../utils/imageApi';
-import { selectLightboxUtil } from '../../utils/imageUtils';
 import './process.scss';
 
 
@@ -30,7 +28,6 @@ class ProgressGallery extends React.Component {
 
 
   updateImages = refKey => {
-    // console.log('updating process images for #' + refKey);
     onsitePhotos(refKey)
       .then(resources => {
         this.setState({
@@ -42,42 +39,46 @@ class ProgressGallery extends React.Component {
 
   render () {
     const { progressImages } = this.state;
-    const { selectLightbox, imageHeight } = this.props;
-    return (
-      <CloudinaryContext cloudName="cantimaginewhy">
-      <div className="process-images">
-        {progressImages.length === 0 && (
-          <h4>No progress images found for this item.</h4>
-        )}
-        {progressImages.map(image => (
-          <Image 
-            key={image.public_id}
-            title={image.context.custom.caption || image.public_id.split('-').pop()}
-            className="responsive thumbnail" 
-            publicId={image.public_id} 
-            height={imageHeight}
-            onClick={() => selectLightbox(image.public_id)}
-          >
-            <Transformation height={imageHeight} crop="thumb" />
-            <Transformation defaultImage={defaultCPI} />
-          </Image>
-        ))}     
-      </div>
-      </CloudinaryContext>
-    )
+    const { selectImage, imageHeight } = this.props;
+    if (progressImages.length > 0) {
+      return (
+        <CloudinaryContext cloudName="cantimaginewhy">
+        <div className="process-images">
+          <div className="process-label">
+            <span className="label">View Artistic Process</span>
+          </div>
+          {progressImages.map(image => (
+            <Image 
+              key={image.public_id}
+              title={image.context.custom.caption || image.public_id.split('-').pop()}
+              responsive
+              publicId={image.public_id} 
+              height={imageHeight}
+              onClick={() => selectImage(image.public_id)}
+            >
+              <Transformation height={imageHeight} crop="thumb" />
+              <Transformation defaultImage={defaultCPI} />
+            </Image>
+          ))}     
+        </div>
+        </CloudinaryContext>
+      )
+    } else {
+      return <div className="no-progress" />
+    }
+    
   }
 }
 
 ProgressGallery.propTypes = {
   refKey: PropTypes.string.isRequired,
   imageHeight: PropTypes.number.isRequired,
-  selectLightbox: PropTypes.func.isRequired
+  selectImage: PropTypes.func.isRequired
 }
 
 ProgressGallery.defaultProps = {
   refKey: 'missing_key',
-  imageHeight: 80,
-  selectLightbox: selectLightboxUtil
+  imageHeight: 80
 }
 
 export default ProgressGallery;

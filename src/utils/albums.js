4d102dfd65596282a145df39c1a5ad0bd8381626
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-10 19:38:50 -0400

    create albumGroups, sortField

--- src/utils/albums.js
+++ src/utils/albums.js
@@ -1,51 +1,90 @@
 // albums.js
-export const albums = [
+export const albumGroups = [
   {
-    "name": "Plein Air",
-    "tag": "en plein air",
-    "thumbnail": "art/early_fall_mt_feake",
-    "description": "Painted on site"
+    "name": "Recent (2016 - Present)",
+    "albums": [
+      {
+        "name": "Plein Air",
+        "tag": "en plein air",
+        "thumbnail": "art/early_fall_mt_feake",
+        "description": "Painted on site",
+        "sortField": ".alb-order"
+      },
+      {
+        "name": "Scenery",
+        "tag": "scenery-new",
+        "thumbnail": "art/midwinter",
+        "description": "Painted from a photograph",
+        "sortField": ".year"
+      },
+      {
+        "name": "Portrait",
+        "tag": "portrait-new",
+        "thumbnail": "nfs/luthien-5",
+        "description": "Recent attempts at portraiture",
+        "sortField": ".alb-order"
+      },
+      {
+        "name": "Watchmen",
+        "tag": "watchmen",
+        "thumbnail": "nfs/watchmen",
+        "description": "Suddenly inspired to draw some comic book characters",
+        "sortField": ".alb-order"
+      },
+      {
+        "name": "Name Tags",
+        "tag": "nametag",
+        "thumbnail": "nfs/charlie-3_33523261796_o_njzr8m",
+        "description": "Name tags I made for various people, mostly me",
+        "sortField": ".year"
+      },
+      {
+        "name": "Name Plates",
+        "tag": "illumination",
+        "thumbnail": "nfs/michele-cantara_2018",
+        "description": "Like name tags, but bigger",
+        "sortField": ".year"
+      },
+    ]
   },
   {
-    "name": "Scenery",
-    "tag": "scenery",
-    "thumbnail": "art/midwinter",
-    "description": "Painted from a photograph"
-  },
-  {
-    "name": "Knotwork",
-    "tag": "knotwork",
-    "thumbnail": "art/lotus",
-    "description": "Celtic-inspired artwork"
-  },
-  {
-    "name": "People",
-    "tag": "portrait",
-    "thumbnail": "nfs/rihanna_26805063606_o_guugen",
-    "description": "Sketches of people; some of them famous"
-  },
-  {
-    "name": "Plaitwork",
-    "tag": "plaitwork",
-    "thumbnail": "nfs/mandelbrot-set",
-    "description": "My first type of artwork; like knotwork, but with all right angles"
-  },
-  {
-    "name": "Name Tags",
-    "tag": "nametag",
-    "thumbnail": "nfs/charlie-3_33523261796_o_njzr8m",
-    "description": "Name tags I made for various people, mostly me"
-  },
-  {
-    "name": "Name Plates",
-    "tag": "illumination",
-    "thumbnail": "nfs/michele-cantara_2018",
-    "description": "Like name tags, but bigger"
-  },
-  {
-    "name": "Archive",
-    "tag": "archive",
-    "thumbnail": "nfs/ferry-beach-rock-painting_41932404964_o_lf4uhn",
-    "description": "All the other stuff"
+    "name": "Older (2010 - 2015)",
+    "albums": [
+      {
+        "name": "Scenery",
+        "tag": "scenery-old",
+        "thumbnail": "art/salisbury-pond_2011",
+        "description": "Painted from a photograph",
+        "sortField": ".year"
+      },
+      {
+        "name": "Knotwork",
+        "tag": "knotwork",
+        "thumbnail": "art/lotus",
+        "description": "Celtic-inspired artwork",
+        "sortField": ".caption"
+      },
+      {
+        "name": "People",
+        "tag": "portrait-old",
+        "thumbnail": "nfs/rihanna_26805063606_o_guugen",
+        "description": "Sketches of people; some of them famous",
+        "sortField": ".caption"
+      },
+      {
+        "name": "Plaitwork",
+        "tag": "plaitwork",
+        "thumbnail": "nfs/mandelbrot-set",
+        "description": "My first type of artwork; like knotwork, but with all right angles"
+      },
+      {
+        "name": "Archive",
+        "tag": "archive",
+        "thumbnail": "nfs/ferry-beach-rock-painting_41932404964_o_lf4uhn",
+        "description": "All the other stuff"
+      }
+    ]
   }
 ];
+
+export const albums = [];

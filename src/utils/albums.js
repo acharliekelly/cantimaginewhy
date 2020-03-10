698dce25dbbaef2072aafa67efd78d3c31f15e66
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-10 04:35:59 -0400

    adjust albums

--- src/utils/albums.js
+++ src/utils/albums.js
@@ -22,23 +22,29 @@ export const albums = [
     "name": "People",
     "tag": "portrait",
     "thumbnail": "nfs/rihanna_26805063606_o_guugen",
-    "description": "Sketches of people"
+    "description": "Sketches of people; some of them famous"
+  },
+  {
+    "name": "Plaitwork",
+    "tag": "plaitwork",
+    "thumbnail": "nfs/mandelbrot-set",
+    "description": "My first type of artwork; like knotwork, but with all right angles"
   },
   {
     "name": "Name Tags",
     "tag": "nametag",
     "thumbnail": "nfs/charlie-3_33523261796_o_njzr8m",
-    "description": "Name tags I made for various people"
+    "description": "Name tags I made for various people, mostly me"
   },
   {
-    "name": "Illustrated Name Plates",
+    "name": "Name Plates",
     "tag": "illumination",
     "thumbnail": "nfs/michele-cantara_2018",
     "description": "Like name tags, but bigger"
   },
   {
     "name": "Archive",
-    "tag": "nfs",
+    "tag": "archive",
     "thumbnail": "nfs/ferry-beach-rock-painting_41932404964_o_lf4uhn",
     "description": "All the other stuff"
   }

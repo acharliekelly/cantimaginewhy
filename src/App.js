4c1b9a70deb168d9a338f534014500746e4c5fe8
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-10 04:35:32 -0400

    replace lightbox with ImageZoom

--- src/App.js
+++ src/App.js
@@ -7,38 +7,50 @@ import {
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faChevronCircleLeft, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
-import Lightbox from 'react-image-lightbox';
+// import Lightbox from 'react-image-lightbox';
 
 import Menu from './components/Menu/';
 import ContactPage from './views/Contact/';
 import Footer from './components/Footer/';
 import HomePage from './views/Home/';
 import AboutPage from './views/About/';
-import ArtworkPage from './views/Artwork';
-import { lightboxImageSrc } from './utils/imageApi';
+import ArtworkPage from './views/Artwork/';
+// import { lightboxImageSrc } from './utils/imageApi';
+import ImageZoom from './components/ImageZoom/';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.scss';
-import 'react-image-lightbox/style.css';
-
-
 
 
 const App = () => {
   const [ lightboxOpen, setLightboxOpen ] = useState(false);
   const [ selectedImageId, setSelectedImageId ] = useState('');
+  const [ lightboxImages, setLightboxImages ] = useState([]);
+  const [ lightboxCurrentIndex, setLightboxCurrentIndex ] = useState(0);
 
   /**
    * takes CPI from any child component, opens lightbox
    * @param {*} imageId 
    */
-  const selectLightboxImage = imageId => {
-    setSelectedImageId(imageId);
+  const selectLightboxImage = (imageId, images = [], currentIndex = -1) => {
+    if (images.length > 0) {
+      setLightboxImages(images);
+      let idx = currentIndex;
+      if (idx < 0) {
+        idx = images.findIndex(img => img.public_id === imageId);
+      }
+      setLightboxCurrentIndex(idx);
+    } else {
+      setSelectedImageId(imageId);
+    }
+    console.log('opening Zoom')
     setLightboxOpen(true);
   }
 
   const closeLightbox = () => {
     setSelectedImageId('');
+    setLightboxImages([]);
+    setLightboxCurrentIndex(0);
     setLightboxOpen(false);
   }
 
@@ -66,13 +78,17 @@ const App = () => {
         </div>
       </Router>
       <Footer />
+
       { lightboxOpen && (
-        <Lightbox 
-          mainSrc={lightboxImageSrc(selectedImageId)}
-          onCloseRequest={closeLightbox}
-          discourageDownloads
-      />
+        <ImageZoom 
+          imageList={lightboxImages}
+          selectedIndex={lightboxCurrentIndex}
+          selectedImageId={selectedImageId}
+          closeLightbox={closeLightbox}
+        />
       )}
+      
+       
     </div>
   );
 }

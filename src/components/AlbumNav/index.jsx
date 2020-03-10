7da65e30f09bf7513debeda12da4f966fa8e28fa
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-10 19:37:10 -0400

    got working with albumGroups

--- src/components/AlbumNav/index.jsx
+++ src/components/AlbumNav/index.jsx
@@ -1,49 +1,72 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
+import Nav from 'react-bootstrap/Nav';
 import { defaultImg } from '../../utils/imageApi';
-import { albums } from '../../utils/albums';
+import { albumGroups } from '../../utils/albums';
 
-import '../FilterNav/filter-nav.scss';
+import './nav.scss';
 
 class AlbumNav extends Component {
-
   constructor (props) {
     super(props);
-
     this.state = {
+      groupIndex: 0,
+      navAlbums: [],
       selectedNav: null
     }
   }
 
   componentDidMount () {
-    
+    this.groupSelect(0);
+
   }
 
-  handleNavClick = nav => {
+  navClick = nav => {
     // nav is object (album) from album list
-    // { name, tag, thumbnail, description }
+    // { name, tag, thumbnail, description, sortField }
     this.setState({
       selectedNav: nav
-    });
+    })
+    this.props.handleNavChange(nav);
+  }
 
-    this.props.handleNavChange(nav.tag);
+  groupSelect = eventKey => {
+    this.props.handleClearGallery();
+    this.setState({
+      groupIndex: eventKey,
+      navAlbums: albumGroups[eventKey].albums,
+      selectedNav: null
+    })
+    
   }
 
   render () {
-    const { selectedNav } = this.state;
-    
+    const { navAlbums, groupIndex, selectedNav } = this.state;
     return (
       <CloudinaryContext cloudName="cantimaginewhy">
-        
+        <Nav 
+            variant="pills" 
+            defaultActiveKey={groupIndex} 
+            className="gallery-nav justify-content-center" 
+            onSelect={this.groupSelect}
+          >
+            <span className="label">Albums:</span>
+            {albumGroups.map((group, index) => (
+              <Nav.Item key={index}>
+                <Nav.Link eventKey={index}>{group.name}</Nav.Link>
+              </Nav.Item>
+            ))}
+        </Nav>
+
         <div className="album-list">
-          {albums.map(album => {
+          {navAlbums && navAlbums.map(album => {
               let cls = 'album-btn responsive thumbnail';
               if (selectedNav && selectedNav.tag === album.tag) {
                 cls += ' selected-nav'
               }
               return (
-                <div key={album.tag} id={album.tag} className={cls} onClick={() => this.handleNavClick(album)}>
+                <div key={album.tag} id={album.tag} className={cls} onClick={() => this.navClick(album)}>
                     <Image  
                         publicId={`${album.thumbnail}`}
                         dpr="auto"
@@ -66,12 +89,13 @@ class AlbumNav extends Component {
         )}
       </CloudinaryContext>
     );
+        
   }
 }
 
 AlbumNav.propTypes = {
   handleNavChange: PropTypes.func.isRequired,
-  handleClearGallery: PropTypes.func
+  handleClearGallery: PropTypes.func.isRequired
 };
 
 export default AlbumNav;

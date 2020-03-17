a66eec0b36666ff7d63d65d624477abb6497c2a0
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-16 23:28:07 -0400

    gallery types

--- src/views/Artwork/index.jsx
+++ src/views/Artwork/index.jsx
@@ -6,16 +6,63 @@ import ImageGallery from '../../components/ImageGallery';
 
 import './artwork.scss';
 
+// FILTER = 0, ALBUM = 1
+
+const galleryDescriptions = [
+  'Filters artwork by commonalities, such as medium or location. One image can appear in multiple categories, and only includes most recent work.',
+  'Group artwork by somewhat arbitrary Album names, but each image should appear in only one album. Includes early work left out of filters.'
+]
+
+const getType = ev => {
+  return parseInt(ev.target.id.split('-')[1])
+}
 
 const ArtworkPage = props => {
   const [ galleryType, setGalleryType ] = useState(0);
+  const [ currentLabel, setCurrentLabel ] = useState(0);
+  
+  const typeClass = type => {
+    let cls = 'option';
+    if (type === galleryType) cls += ' selected';
+    return cls;
+  }
+
+  const typeSwitch = ev => {
+    setGalleryType(getType(ev))
+  }
+
+  // set description to hovered type
+  const typeHover = ev => {
+    const type = getType(ev);
+    return setCurrentLabel(type);
+  }
+
+  // revert description to gallery type
+  const typeExit = ev => {
+    setCurrentLabel(galleryType)
+  }
 
   return (
     <div className="content">
       <div className="gallery-switch">
         <span className="label">Gallery: </span>
-        <span className={'option' + (galleryType===0 ? ' selected' : '')} onClick={() => setGalleryType(0)}>Filters</span>
-        <span className={'option' + (galleryType===1 ? ' selected' : '')} onClick={() => setGalleryType(1)}>Albums</span>
+        <span 
+          id="gt-0" 
+          className={typeClass(0)} 
+          onClick={typeSwitch}
+          onMouseEnter={typeHover}
+          onMouseLeave={typeExit}
+          >Filters</span>
+        <span 
+          id="gt-1" 
+          className={typeClass(1)} 
+          onClick={typeSwitch}
+          onMouseEnter={typeHover}
+          onMouseLeave={typeExit}
+          >Albums</span>
+        <span className="gallery-description">
+          {galleryDescriptions[currentLabel]}
+        </span>
       </div>
       <ImageGallery 
         selectLightbox={props.selectLightbox} 

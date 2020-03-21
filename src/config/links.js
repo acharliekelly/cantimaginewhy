d25ed8f0ea2636a0026d73e1073d87b249caed7c
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-21 17:36:24 -0400

    move link content to config file

---
+++ src/config/links.js
@@ -0,0 +1,30 @@
+export const links = [
+  {
+    name: 'Instagram',
+    lib: 'fab',
+    icon: 'instagram',
+    url: 'https://instagram.com/cant_imagine_why'
+  },
+  {
+    name: 'Facebook',
+    lib: 'fab',
+    icon: 'facebook',
+    url: 'https://facebook.com/acharliekelly'
+  },
+  {
+    name: 'Github',
+    lib: 'fab',
+    icon: 'github',
+    url: 'https://github.com/acharliekelly'
+  },
+  // {
+  //   name: 'Shop',
+  //   icon: 'shopping-cart',
+  //   url: 'https://charlie-kelly.pixels.com'
+  // },
+  {
+    name: 'Email',
+    icon: 'paper-plane',
+    url: 'mailto:cant.imagine.why00@gmail.com'
+  }
+];

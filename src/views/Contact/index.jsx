bcf1d03b3e2415edcf1873f0623022fcaad44635
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-21 17:35:10 -0400

    use contact links instead of form

--- src/views/Contact/index.jsx
+++ src/views/Contact/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { ContactForm } from '../../components/ContactForm'
+// import Container from 'react-bootstrap/Container';
+
+import ContactLinks from '../../components/ContactLinks/';
+
+import './contact.scss';
 
 const ContactPage = props => (
   <div className="content">
     
-    <main>
-      <ContactForm />
+    <main className="contact">
+      
+      <div className="contact-links">
+        <ContactLinks layout="vert" displayType="both" size={3} />
+      </div>
+      
     </main>
   </div>
 );

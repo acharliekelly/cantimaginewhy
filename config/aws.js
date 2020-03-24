30541c07cd6826ffcf91441523930ad0b1cf4ff2
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-23 23:26:16 -0400

    not useful for this project

--- config/aws.js
+++
@@ -1,3 +0,0 @@
-export const RDS_ENDPOINT_URL = 'cantimagine-rdb.cpccvbmzdc2j.us-east-2.rds.amazonaws.com';
-export const RDS_DBNAME = 'cantimagine-rds';
-export const RDS_USER = 'cantimadmin';
--- curl-scripts/images/metadata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl "https://${API_KEY}:${API_SECRET}@api.cloudinary.com/v1_1/cantimaginewhy/metadata_fields/"
-
-echo

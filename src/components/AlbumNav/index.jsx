86e435460840ccca98678438d0aceef7e179a689
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-24 21:24:54 -0400

    use flat album array, allow switching to filter

--- src/components/AlbumNav/index.jsx
+++ src/components/AlbumNav/index.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
 import { defaultImg } from '../../utils/imageApi';
-import { albumGroups, navDescription } from '../../config/albums';
+import { albums, navDescription } from '../../config/albums';
 
 import '../../css/nav.scss';
 
@@ -17,125 +18,83 @@ class AlbumNav extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      groupIndex: 0,
-      hoverIndex: -1,
-      selectedNav: null,
-      explanOpen: true
+      selectedNav: null
     }
   }
 
-  hoverOnGroup = index => {
-    this.setState({
-      hoverIndex: index
-    });
-  }
-
-  hoverOff = ev => {
-    this.setState({
-      hoverIndex: -1
-    });
-  }
-
   componentDidMount () {
-    this.selectGroup(0);
-
+    // this.selectGroup(0);
+    this.props.updateClearGallery();
   }
 
   selectItem = nav => {
     // nav is object (album) from album list
     // { name, tag, thumbnail, description, sortField }
     this.setState({
-      selectedNav: nav,
-      explanOpen: false
+      selectedNav: nav
     })
     this.props.updateSelectNav(nav);
   }
 
-  selectGroup = eventKey => {
-    this.props.updateClearGallery();
-    this.setState({
-      groupIndex: eventKey,
-      selectedNav: null,
-      hoverIndex: -1
-    })
-    
-  }
-
-  toggleExplanation = () => {
-    const { explanOpen } = this.state;
-    this.setState({
-      explanOpen: !explanOpen
-    })
-  }
 
   render () {
-    const { groupIndex, selectedNav, hoverIndex, explanOpen } = this.state;
-    const activeGroup = albumGroups[groupIndex];
-    const helpBtnVar = explanOpen ? 'info' : 'outline-info';
+    const { selectedNav } = this.state;
+    
     return (
       <CloudinaryContext cloudName="cantimaginewhy">
-         <Navbar className="category-bar justify-content-between">
-          <Button className="help-btn" variant={helpBtnVar} onClick={this.toggleExplanation}>
-            <FontAwesomeIcon icon="question-circle" />
+         <Navbar className="category-bar justify-content-around">
+
+          <Button className="nav-switch" variant="outline-dark" onClick={this.props.updateSwitch}>
+            <FontAwesomeIcon icon="filter" title="Browse by Filter" />
           </Button>
-          <Navbar.Collapse className="justify-content-end">
-            <Nav className="filters" 
-                variant="pills" 
-                defaultActiveKey={0}  
-                onSelect={this.selectGroup}
-              >
-              <Navbar.Text className="label">Filter By:&nbsp;</Navbar.Text>
-              {albumGroups.map((group, index) => (
-                <Nav.Item 
-                  key={index}
-                  onMouseEnter={() => this.hoverOnGroup(index)} 
-                  onMouseLeave={this.hoverOff}>
-                  <Nav.Link eventKey={index}>{group.name}</Nav.Link>
-                </Nav.Item>
-                )
-              )}
-            </Nav>
-          </Navbar.Collapse>
-          <Navbar.Collapse className="justify-content-end category-desc">
-            <Navbar.Text className="active-desc">{albumGroups[groupIndex].description}</Navbar.Text>
-            {hoverIndex >= 0 && (
-            <Navbar.Text className="hover-desc">{albumGroups[hoverIndex].description}</Navbar.Text>
-            )}
-          </Navbar.Collapse>
+
+          <Navbar.Text>
+            <span className="browse-title">Browse by Album</span>
+          </Navbar.Text>
+              
+
+          <OverlayTrigger trigger="click" placement="bottom" overlay={
+            <Popover>
+              <Popover.Title>Albums</Popover.Title>
+              <Popover.Content>{navDescription}</Popover.Content>
+            </Popover>
+          }>
+            <Button className="help-btn" variant="outline-info">
+              <FontAwesomeIcon icon="question-circle" />
+            </Button>
+          </OverlayTrigger>
+        
         </Navbar>
 
         <Container fluid expand="lg" className="album-bar justify-content-between">
           <Row>
             <Col md={3}>
-              {explanOpen ? (
-                <div className="explain-box">
-                  <span className="explanation">{navDescription}</span>
-                </div>
-              ) : (
-                <div className="explain-closed">
-                {selectedNav && (
-                  <div className="current-item">
-                    <div className="nav-title">{selectedNav.name}</div>
-                    <div className="nav-description">{selectedNav.description}</div>
-                  </div>
-                )}
-                </div>
-              )}
+            {selectedNav && (
+              <div className="current-item">
+                <div className="nav-title">{selectedNav.name}</div>
+                <div className="nav-description">{selectedNav.description}</div>
+              </div>
+            )}
             </Col>
             <Col md={6} className="album-col">
               <ul className="albums">
-                {activeGroup && activeGroup.albums.map(album => {
+                {albums.map((album, index) => {
                   let cls = 'album-btn responsive thumbnail';
                   if (selectedNav && selectedNav.tag === album.tag) {
                     cls += ' selected-nav'
                   }
                   return (
-                    <li key={album.tag} id={album.tag} className={cls} onClick={() => this.selectItem(album)}>
-                        <Image publicId={`${album.thumbnail}`}>
-                            <Transformation defaultImage={defaultImg} />
-                            <Transformation height="80" width="80" crop="fill" />   
-                        </Image>
-                        <div className="album-name">{album.name}</div>
+                    <li 
+                      key={index} 
+                      id={album.tag} 
+                      className={cls} 
+                      onClick={() => this.selectItem(album)}
+                    >
+                      <Image publicId={`${album.thumbnail}`}>
+                        <Transformation defaultImage={defaultImg} />
+                        <Transformation height="80" width="80" crop="fill" />   
+                      </Image>
+                      <div className="album-name">{album.name}</div>
                     </li>
                   );
                 })}
@@ -154,7 +113,8 @@ class AlbumNav extends Component {
 
 AlbumNav.propTypes = {
   updateSelectNav: PropTypes.func.isRequired,
-  updateClearGallery: PropTypes.func
+  updateClearGallery: PropTypes.func.isRequired,
+  updateSwitch: PropTypes.func.isRequired
 };
 
 export default AlbumNav;

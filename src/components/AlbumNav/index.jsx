b996f93ab094d9426430f707fa30c31f872ba5d7
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-25 03:39:04 -0400

    fix layout issues

--- src/components/AlbumNav/index.jsx
+++ src/components/AlbumNav/index.jsx
@@ -38,26 +38,26 @@ class AlbumNav extends Component {
 
   render () {
     const { selectedNav } = this.state;
-    
+    const { thumbnailHeight } = this.props;
     return (
       <CloudinaryContext cloudName="cantimaginewhy">
-         <Navbar className="category-bar justify-content-around">
+         <Navbar className="category-bar justify-content-between">
 
           <Button className="nav-switch" variant="outline-dark" onClick={this.props.updateSwitch}>
-            <FontAwesomeIcon icon="filter" title="Browse by Filter" />
+            <FontAwesomeIcon icon="filter" title="Browse by Filter" size="2x" />
           </Button>
 
           <Navbar.Text>
             <span className="browse-title">Browse by Album</span>
           </Navbar.Text>
               
-          <HelpButton header="Albums" content={navDescription} />
+          <HelpButton header="Albums" content={navDescription} size={2} />
         
         </Navbar>
 
         <Container fluid expand="lg" className="album-bar justify-content-between">
           <Row>
-            <Col md={3}>
+            <Col md={2}>
             {selectedNav && (
               <div className="current-item">
                 <div className="nav-title">{selectedNav.name}</div>
@@ -65,7 +65,7 @@ class AlbumNav extends Component {
               </div>
             )}
             </Col>
-            <Col md={6} className="album-col">
+            <Col md={8} className="album-col">
               <ul className="albums">
                 {albums.map((album, index) => {
                   let cls = 'album-btn responsive thumbnail';
@@ -81,7 +81,7 @@ class AlbumNav extends Component {
                     >
                       <Image publicId={`${album.thumbnail}`}>
                         <Transformation defaultImage={defaultImg} />
-                        <Transformation height="80" width="80" crop="fill" />   
+                        <Transformation height={thumbnailHeight} width={thumbnailHeight} crop="fill" />   
                       </Image>
                       <div className="album-name">{album.name}</div>
                     </li>
@@ -89,7 +89,7 @@ class AlbumNav extends Component {
                 })}
               </ul>
             </Col>
-            <Col md={3} className="desc-col">
+            <Col md={2} className="empty">
             
             </Col>
           </Row>
@@ -103,7 +103,12 @@ class AlbumNav extends Component {
 AlbumNav.propTypes = {
   updateSelectNav: PropTypes.func.isRequired,
   updateClearGallery: PropTypes.func.isRequired,
-  updateSwitch: PropTypes.func.isRequired
+  updateSwitch: PropTypes.func.isRequired,
+  thumbnailHeight: PropTypes.number
 };
+
+AlbumNav.defaultProps = {
+  thumbnailHeight: 80
+}
 
 export default AlbumNav;

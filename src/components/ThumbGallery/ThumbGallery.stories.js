cdd41d5e5bef492a4a5d5dd169447db1f784f51b
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-27 01:53:31 -0400

    add header; update thumb size on load

--- src/components/ThumbGallery/ThumbGallery.stories.js
+++ src/components/ThumbGallery/ThumbGallery.stories.js
@@ -9,6 +9,11 @@ import './gallery.scss';
 
 const images = sortByField(resources, '.caption');
 
+const mockNav = {
+  name: 'Acrylic',
+  description: 'Layered acrylic paints'
+}
+
 const mockSelect = index => {
   console.log(images[index].public_id + ' selected');
 }
@@ -36,6 +41,14 @@ stories
     selectThumbnail={mockSelect}
     thumbSize={80}
   />))
+  .add('Heading', () => (
+    <ThumbGallery 
+        galleryImages={images} 
+        selectThumbnail={mockSelect} 
+        thumbSize={60}
+        heading={mockNav}
+      />
+  ))
   .add('Knobs', () => {
     const sizes = number('Thumbnail Size', 100, sizeCtrl);
     const selected = number('Selected', 0, indexCtrl);

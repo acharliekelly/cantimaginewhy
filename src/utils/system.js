a06802795b806545e181443e6f7c535c8d3c76f9
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-27 01:34:22 -0400

    change favicon based on env

--- src/utils/system.js
+++ src/utils/system.js
@@ -1,16 +1,34 @@
 // System Utilities
 
-
-
-export const allowDevMode = () => {
-  return (process.env.NODE_ENV === 'development');
-}
-
 const envIcons = {
   'development': 'cog',
   'test': 'check',
   'production': 'heart'
 }
+
+const favicons = {
+  'development': 'ciw4.png',
+  'test': 'ciw1.png',
+  'production': 'ck.gif'
+};
+
+const getFavicon = () => {
+  return favicons[process.env.NODE_ENV];
+}
+
+const faviconUrl = () => {
+  return process.env.PUBLIC_URL + '/' + getFavicon();
+}
+
+export const updateFavicon = () => {
+  const faviconElem = document.getElementById('favicon');
+  faviconElem.href = faviconUrl();
+}
+
+export const allowDevMode = () => {
+  return (process.env.NODE_ENV === 'development');
+}
+
 
 export const getEnvIcon = () => {
   return envIcons[process.env.NODE_ENV];

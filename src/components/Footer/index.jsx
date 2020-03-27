f8cf4279f301ee38ea6f490be80500f66847eaa5
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-27 15:45:30 -0400

    move EnvMode to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import EnvMode from '../EnvMode';
 
 import './footer.scss';
 
-const Footer = () => (
+const Footer = props => (
   <div className="footer">
     <span className="copyright">&copy;2020 by Charlie Kelly</span>
+    <EnvMode type="icon" devMode={props.devMode} />
   </div>
 );
+
+Footer.propTypes = {
+  devMode: PropTypes.func
+}
 
 export default Footer;

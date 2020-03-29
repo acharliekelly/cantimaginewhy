e4d65ca74ee52c6f15515bfba47e75ebdfcdad60
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-29 13:01:27 -0400

    add content

--- src/config/albums.js
+++ src/config/albums.js
@@ -1,19 +1,40 @@
 // albums.js
+/**
+ * Basic navigation object:
+ * - name: Title
+ * - tag: search slug for request
+ * - thumbnail: example image
+ * - description: descriptive text
+ * - sortField: field to sort results on
+ *   - 'field' = item.field
+ *   - '.field' = item.context.custom.field
+ * TODO:
+ * - display: which components to show
+ *   - default is [ gallery, detail ]
+ *   - only necessary if it needs more
+ *   - options: gallery || detail || progress || explan
+ *   - 'progress' means always show progress
+ *   - '-progress' means never show progress
+ */
 
 export const albums = [
   {
     "name": "Plein Air",
-    "tag": "en plein air",
+    "tag": "plein-air-acrylic",
     "thumbnail": "art/early_fall_mt_feake",
     "description": "Painted on site",
-    "sortField": ".alb-order"
+    "sortField": ".alb-order",
+    "display": [
+      "gallery", "detail", "progress", "explan"
+    ]
   },
   {
     "name": "Scenery",
     "tag": "scenery",
     "thumbnail": "art/midwinter",
     "description": "From photographs of landscapes",
-    "sortField": ".year"
+    "sortField": ".year",
+    "display": [ "-progress" ]
   },
   {
     "name": "Portrait",
@@ -23,18 +44,38 @@ export const albums = [
     "sortField": ".alb-order"
   },
   {
+    "name": "Logos",
+    "tag": "logo",
+    "thumbnail": "ciw4",
+    "description": "Different logos I've made, either as monograms or specifically for this site",
+    "sortField": ".alb-order"
+  },
+  {
+    "name": "Sketch",
+    "tag": "sketch",
+    "thumbnail": "nfs/davis-t",
+    "description": "Onsite pencil sketches of local landmarks",
+    "sortField": ".alb-order"
+  },
+  {
     "name": "Name Tags",
     "tag": "nametag",
     "thumbnail": "nfs/charlie-7_37671079756_o_fvboho",
     "description": "Name tags I made for various people, mostly me",
-    "sortField": ".year"
+    "sortField": ".year",
+    "display": [
+      "gallery", "detail", "explan"
+    ]
   },
   {
     "name": "Name Plates",
     "tag": "illumination",
     "thumbnail": "nfs/emrys_2011",
     "description": "Like name tags, but bigger",
-    "sortField": ".alb-order"
+    "sortField": ".alb-order",
+    "display": [
+      "gallery", "detail", "explan"
+    ]
   },
   {
     "name": "Knotwork",
@@ -51,17 +92,30 @@ export const albums = [
     "sortField": '.year'
   },
   {
-    "name": "People",
+    "name": "AANE",
+    "tag": "aane",
+    "thumbnail": "nfs/golden",
+    "description": "A series of the letters A-A-N-E, arranged artistically",
+    "sortField": ".alb-order",
+    "display": [
+      "gallery", "detail", "explan"
+     ]
+  },
+  {
+    "name": "Celebrities",
     "tag": "portrait-old",
-    "thumbnail": "nfs/rihanna_26805063606_o_guugen",
+    "thumbnail": "nfs/rihanna",
     "description": "Sketches of people; some of them famous",
-    "sortField": ".caption"
+    "sortField": ".caption",
+    "display": [
+      "gallery", "detail", "explan"
+     ]
   },
   {
     "name": "Plaitwork",
     "tag": "plaitwork",
     "thumbnail": "nfs/mandelbrot-set",
-    "description": "My first type of artwork; like knotwork, but with all right angles"
+    "description": "My first type of artwork; like knotwork, but with all right angles. Looks like placemats."
   },
   {
     "name": "Archive",
@@ -71,4 +125,6 @@ export const albums = [
   }
 ];
 
-export const navDescription = `Here is all my art, organized into albums.`;
+export const navDescription = `This is my entire collection of artwork from the past 
+10 years or so, grouped by album. A lot of it doesn't have any other information,
+such as Year or Title, so it can't be easily filtered.`;

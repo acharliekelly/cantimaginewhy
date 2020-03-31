3abd83dba9df5f604cee8cec0073ab294aeaa409
Charlie Kelly <charliekelly31415@gmail.com>
2020-03-30 23:11:56 -0400

    source of About Album text

---
+++ src/config/text.js
@@ -0,0 +1,72 @@
+// This is a JS file full of text. Text-in-JS.
+
+/**
+ * Format:
+ * - tagName: explanText
+ */
+export const albumExplanations = {
+  "plein-air-acrylic": `
+  In the summer of 2019, I bought a portable easil that I could carry on my bike, along with my other
+  art supplies, and started doing acrylic landscape paintings of local landmarks. By choosing scenes adjacent
+  to public thoroughfares, I would get a pleasant stream of passers-by, complimenting my work.`,
+
+  "scenery": `While I have a lot of pictures that could technically be classified as Scenery, these are 
+  specifically the ones I did not create on site, but rather from a photograph. At least, in many cases, it was a 
+  photo that I personally took, with the specific intention of painting it later.`,
+
+  "portrait-new": `After a long break from portraiture, I decided to give it another try this winter (2019),
+  once it got too cold for my Plein Air painting. The first group is a friend who photographs well.`,
+
+  "logo": `Making logos is something I just do in my free time, whether or not a I need a logo for something.
+  Mostly what I'm making is calligraphic monograms - artistic arrangements of initials. I've come up with a lot
+  of them for the letters 'CIW', and also 'CK'.`,
+
+  "sketch": `This was my first run at creating art on site, and requires a lot less packing than acrylic painting.
+  I'm happy if I can make a landmark recognizable to someone who's seen it before.`,
+
+  "nametag": `I've been involved in several different groups that use name tags. A lot of them use single-use 
+  stickers (the ones that say "Hello My Name Is" at the top), and that's why I taught myself calligraphy (pays 
+  off every time I go to a Meetup). But some groups use permanent name tags, and I always make a habit of making
+  mine really, really nice. Which inevitably leads to other participants asking if I can make them one, too.
+  Which of course I can, and do. The only contstraints are the size of the plastic name tag holders used by the
+  specific group, and what information they require (eg just first name, or first, last, and preferred pronoun).
+  I generally try to get their favorite color, too; just so the tag is more likely to go with their outfit.`,
+
+  "illumination": `I started making Name Plates for people at work who had cubicles and therefore
+  no placard on their office door. And then before I knew it, everybody wanted one. It's a person's name and
+  whatever they're into - the place I worked when it started is a nonprofit for Aspergers, so a lot of people have
+  REALLY strong interests. In this gallery, the interest is in the description field, under the name.`,
+
+  "knotwork": `The first thing I ever wanted to be able to draw was Celtic knotwork. I made several attempts as a
+  child, but without a specific design to copy, my patterns always ended up looking more like intestines. So, when I 
+  took up drawing as an adult, learning to copy knotwork designs was my first goal. Eventually I started making up my
+  own designs that did not immediately remind one of digestion.`,
+
+  "holiday": `Sometimes I make holiday cards. The determining factor is whether or not I think of it at least a month
+  before the holiday, giving me enough time to design, draw, scan, order, receive, and mail the cards. Normally, 
+  I remember the day before the holiday, decide I'm definitely going to make something for the next holiday, and then
+  don't. But these are some designs from the occasions when I did think of it in time.`,
+
+  "aane": `For most of the past decade, I've been affiliated with/employed by an organization called AANE. (What that
+  stands for changed over time, but at least one of the A's is usually Aspergers.) Anyway, at one time, their logo
+  was stunningly dull: just block letters, arranged in a square. I suggested there might be more interesting ways
+  of displaying four letters. And then I proceded to demonstrate.`,
+
+  "portrait-old": ``,
+  "plaitwork": ``,
+  "archive": ``,
+  "pointillist": ``,
+  "triptych": ``,
+  "nightscape": ``,
+  "miniature": ``,
+  "panorama": ``,
+  "era-learning": ``,
+  "early-work": ``,
+  "sabbatical": ``,
+  "era-recent": ``,
+  "era-current": ``,
+  "for-sale": ``,
+  "for-print": ``,
+  "nfs": ``
+};
+

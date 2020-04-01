ad8caeda7b01b28790cf0002ee9d131d968474f2
Charlie Kelly <charliekelly31415@gmail.com>
2020-04-01 16:07:16 -0400

    added more About Album text

--- src/config/text.js
+++ src/config/text.js
@@ -6,16 +6,22 @@
  */
 export const albumExplanations = {
   "plein-air-acrylic": `
-  In the summer of 2019, I bought a portable easil that I could carry on my bike, along with my other
-  art supplies, and started doing acrylic landscape paintings of local landmarks. By choosing scenes adjacent
-  to public thoroughfares, I would get a pleasant stream of passers-by, complimenting my work.`,
+  Early in the summer of 2019, I bought a portable easel that I could carry on my bike, along with my other
+  art supplies, and started doing acrylic landscape paintings of local landmarks. I quickly discovered that 
+  by selecting a vantage point adjacent to a public thoroughfare on a nice day, 
+  I could be treated to a pleasant stream of passers-by complimenting my work. Which turns out to be way more
+  fun than painting from a photo, alone in my studio.`,
 
-  "scenery": `While I have a lot of pictures that could technically be classified as Scenery, these are 
-  specifically the ones I did not create on site, but rather from a photograph. At least, in many cases, it was a 
-  photo that I personally took, with the specific intention of painting it later.`,
+  "scenery": `Unlike my Plein Air landscapes, which I painted on site, these are paintings I made from
+  photographs. However, they were all photographs that I took, with the intention of painting them. That 
+  sets them apart somewhat from pictures I copied from postcards or magazines, in that I at least viewed
+  all of these scenes for myself before deciding they needed to be captured.`,
 
   "portrait-new": `After a long break from portraiture, I decided to give it another try this winter (2019),
-  once it got too cold for my Plein Air painting. The first group is a friend who photographs well.`,
+  once it got too cold for my outdoor painting. Turns out not to be exactly like riding a bike, but I gradually
+  got back into it. 'Luthien' is a friend who photographs well. The others are from a set of Watchmen-inspired
+  artwork I made for my cousin's dorm room - it was the least I could do, after getting him hooked on my
+  favorite comic book franchise.`,
 
   "logo": `Making logos is something I just do in my free time, whether or not a I need a logo for something.
   Mostly what I'm making is calligraphic monograms - artistic arrangements of initials. I've come up with a lot
@@ -35,7 +41,7 @@ export const albumExplanations = {
   "illumination": `I started making Name Plates for people at work who had cubicles and therefore
   no placard on their office door. And then before I knew it, everybody wanted one. It's a person's name and
   whatever they're into - the place I worked when it started is a nonprofit for Aspergers, so a lot of people have
-  REALLY strong interests. In this gallery, the interest is in the description field, under the name.`,
+  really strong interests. In this gallery, the interest is in the description field, under the name.`,
 
   "knotwork": `The first thing I ever wanted to be able to draw was Celtic knotwork. I made several attempts as a
   child, but without a specific design to copy, my patterns always ended up looking more like intestines. So, when I 
@@ -52,21 +58,55 @@ export const albumExplanations = {
   was stunningly dull: just block letters, arranged in a square. I suggested there might be more interesting ways
   of displaying four letters. And then I proceded to demonstrate.`,
 
-  "portrait-old": ``,
-  "plaitwork": ``,
-  "archive": ``,
-  "pointillist": ``,
-  "triptych": ``,
-  "nightscape": ``,
-  "miniature": ``,
-  "panorama": ``,
+  "portrait-old": `Another early artistic interest of mine was portraiture. And a good way to tell if I was getting it right
+  was if my portraits were recognizable. For that to work with strangers, the portrait needs to be of someone universally
+  recognizable, like a celebrity. So for a while in my Early Period, I would buy pop culture magazines and draw anyone I 
+  recognized from them (because if I recogize someone, they have to be really famous).`,
+
+  "plaitwork": `The first artistic style that I really adopted as my own was Plaitwork, a variant of knotwork that ends up
+  looking more like woven-reed placemats than anything else. This was back when I wasn't really comfortable even admitting
+  that "artist" was a term I might like to one day apply to myself. But I was OK with being good at geometry, and at following
+  simple rules. So, I came up with a detailed process that ultimately came down to very simple rules (which I'm going to 
+  demonstrate rather than trying to describe).`,
+
+  "archive": `My 'Miscellaneous' album, with everything that doesn't fit into some other category`,
+
+  "pointillist": `When I started learning to paint with watercolors, I lacked access to quality watercolor paper. The 
+  trouble with putting watercolor on paper that isn't designed for it is that the paper will start to curl as it becomes
+  inconsistently damp. My solution was to apply the watercolor in tiny dots, so no part of the paper was ever wet enough
+  to seriously change the shape or texture. Later, I found out about the French post-impressionist artist George Seurat and
+  the Pointillist movement, though I suspect they got started for different reasons.`,
+
+  "triptych": `A Triptych is a picture on 3 panels (from Greek Tri + Ptuchos, "panel"). Most of these came from my
+  Sabbatical period, as a way of fitting neatly into a standardized envelope without permanently damaging the picture
+  with creases. This way, the creases were in between the panels.`,
+
+  "nightscape": `During my first summer of Plein Air painting, I kept noticing that 1) I spent a lot of time out biking at
+  night, when it was too dark to do my usual type of painting, and 2) the lights of Boston at night, especially looking
+  the river, are spectacular. And then I discovered that both canvas and panel come in black.`,
+
+  "miniature": `At some point, I got it into my head that making smaller paintings would mean less time commitment for 
+  each piece. This turned out not to be the case. In fact, very small paintings took just as long as larger ones; I just
+  had to use a smaller brush. I made a bunch of them, anyway. These are all either on 7" x 5" canvas, or 6" x 6" panels.`,
+
+  "panorama": `Most of these pieces were created during my Sabbatical period, when all my work had to fit neatly into 
+  a standard-sized envelope, because reasons. Since all my paper was 8.5" x 11", there were basically two ways to accomplish
+  this: fold the paper in thirds and make a triptych, or just use an envelope-sized area of the paper. Hence, I have
+  a lot of long thin pictures.`,
   "era-learning": ``,
   "early-work": ``,
-  "sabbatical": ``,
+
+  "sabbatical": `For a lot of my work to make sense, it's important to know that I spent some time with limited access to 
+  high-qaulity artistic materials. The how and why of that experience is not particularly germaine; the relevant parts 
+  are that during one of my most prolific periods, I had very limited access to materials, and also that everything I made
+  on 8.5" x 11" sheets had to fit into a 9" x 4" envelope.`,
+
   "era-recent": ``,
   "era-current": ``,
   "for-sale": ``,
   "for-print": ``,
   "nfs": ``
 };
+
+
 

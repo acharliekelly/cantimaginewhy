a8dca825e42fc7d826e6c42bd69ee1e36f23272d
Charlie Kelly <charliekelly31415@gmail.com>
2020-04-04 01:51:11 -0400

    added shuffle fn for SliceGallery

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -84,3 +84,14 @@ export const faaUrl = productKey => {
     return FAA_URL;
   }
 }
+
+
+// ANOTHER MISCELLANEOUS FUNCTION
+export const shuffleArray = array => {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}

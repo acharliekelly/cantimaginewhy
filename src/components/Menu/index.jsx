8cde4d2a21db01c771864f3926f892e8a58355ac
Charlie Kelly <charliekelly31415@gmail.com>
2020-04-04 01:50:02 -0400

    more responsive

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,11 +4,10 @@ import { NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ContactLinks from '../ContactLinks/';
 import Logo from '../Logo/';
 // import ExternalLink from '../Buttons/ExternalLink';
-import { selectLightboxUtil} from '../../utils/imageUtils';
+
 
 
 import './menu.scss';
@@ -29,7 +28,7 @@ const menuNavs = [
     location: "about",
   },
   {
-    name: "Contact",
+    name: "Connect",
     location: "contact"
   },
   {
@@ -39,43 +38,53 @@ const menuNavs = [
   }
 ];
 
+const externalLnk = nav => (
+  <a href={nav.location} 
+    className="external-link" 
+    target="_blank" 
+    rel="noreferrer noopener">
+      {nav.name}
+    </a>
+)
+
 
 const Menu = props => {
   return (
     <div className="menu-wrapper">
-      <Navbar bg="light" className="justify-content-between">
+      <Navbar bg="light" className="justify-content-between" expand="lg">
         <Navbar.Brand>
-          <Logo selectLightbox={props.selectLightbox} />
+          <Logo {...props} />
         </Navbar.Brand>
-        <Container className="justify-content-end">
-          <Nav >
-            {menuNavs.map((nav, index) => (
-              <Nav.Item key={index}>
-                {nav.external ? (
-                  <a href={nav.location} className="external-link" 
-                    target="_blank" rel="noreferrer noopener">{nav.name}</a>
-                ) : (
-                  <NavLink to={nav.location}>{nav.name}</NavLink>
-                )}
-              </Nav.Item>
-            ))}
-          </Nav>
-        </Container>
-        <Container className="justify-content-end">
-          <ContactLinks displayType="icon" size="2x" horizontal="md" />
-        </Container>
+        <Navbar.Toggle aria-controls="menu-content" />
+        <Navbar.Collapse id="menu-content">
+          <Container className="justify-content-end">
+            <Nav >
+              {menuNavs.map((nav, index) => (
+                <Nav.Item key={index}>
+                  {nav.external ? externalLnk(nav) : (
+                    <NavLink to={nav.location}>{nav.name}</NavLink>
+                  )}
+                </Nav.Item>
+              ))}
+            </Nav>
+          </Container>
+          <Container className="justify-content-end">
+            <ContactLinks 
+              displayType="icon" 
+              size="2x" 
+              horizontal="md" 
+              group="head" />
+          </Container>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
 }
 
 Menu.propTypes = {
-  selectLightbox: PropTypes.func.isRequired,
+  selectLightbox: PropTypes.func,
   devMode: PropTypes.func
 }
 
-Menu.defaultProps = {
-  selectLightbox: selectLightboxUtil
-};
 
 export default Menu;

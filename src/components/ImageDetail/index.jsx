90a568c60b4d3e23e6db5e5c9bad062a6beae5e4
Charlie Kelly <charliekelly31415@gmail.com>
2020-04-06 22:40:54 -0400

    give distinct class names to info fields

--- src/components/ImageDetail/index.jsx
+++ src/components/ImageDetail/index.jsx
@@ -53,32 +53,32 @@ const ImageDetail = props => {
               <div className="title">{info.title}</div>
               <p className="mb-2 text-muted">{info.description}</p>
               {info.completed && (
-                <div className="info">
+                <div className="info completed-field">
                   <span className="label">Completed: </span>
                   <span className="data"><em>{info.dateCompleted.toDateString()}</em></span>
                 </div>
               )}
               {info.location && (
-                <div className="info">
+                <div className="info location-field">
                   <span className="label">Location: </span>
                   <span className="data">{info.location}</span>
                 </div>
               )}
               {info.materialInfo && (
-                <div className="info">
+                <div className="info material-field">
                   <span className="label">Material: </span>
                   <span className="data">{info.medium}, {info.size}</span>
                 </div>
               )}
               {info.year && (
-                <div className="info">
+                <div className="info year-field">
                   <span className="label">Year: </span>
                   <span className="data">{info.year}</span>
                 </div>
               )}
               {info.forSale && (
-                <div className="info">
-                  <span className="label">Price: </span>
+                <div className="info price-field">
+                  <span className="label">Original: </span>
                   <span className="data">${info.price}</span>
                 </div>
               )}

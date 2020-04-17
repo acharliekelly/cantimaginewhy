4c24baeb644e9daf8a7e3ee6bfc6f682487d53a3
Charlie Kelly <charliekelly31415@gmail.com>
2020-04-17 01:45:43 -0400

    add dropdown menu option for small screens

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
 import './menu.scss';
@@ -31,17 +33,18 @@ const menuNavs = [
   }
 ];
 
+
+
 const externalLnk = nav => (
   <a href={nav.location} 
     className="external-link" 
     target="_blank" 
     rel="noreferrer noopener">
       {nav.name}
-    </a>
+  </a>
 )
 
-
-const Menu = props => (
+const NavMenu = () => (
   <Nav className="menu-nav" >
     {menuNavs.map((nav, index) => (
       <Nav.Item key={index}>
@@ -52,5 +55,30 @@ const Menu = props => (
     ))}
   </Nav>
 );
+
+const DropdownMenu = () => {
+  const [ activeKey, setActiveKey ] = useState('Home')
+  return (
+    <DropdownButton 
+      variant="secondary" 
+      className="menu-drop" 
+      title={activeKey}
+      onSelect={setActiveKey}
+      >
+      {menuNavs.map((nav, index) => (
+        <Dropdown.Item 
+          key={index}
+          eventKey={nav.name}
+          active={nav.name === activeKey} 
+          href={nav.external ? nav.location : `#/${nav.location}`}>
+            {nav.name}
+        </Dropdown.Item>
+      ))}
+    </DropdownButton>
+  )
+}
+
+
+const Menu = props => props.type === 'drop' ? <DropdownMenu /> : <NavMenu />
 
 export default Menu;

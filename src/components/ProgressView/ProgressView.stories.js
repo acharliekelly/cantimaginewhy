48d00f19d30b2c922cb11846fa4b4a54f930aca0
Charlie Kelly <charliekelly31415@gmail.com>
2020-04-17 01:56:52 -0400

    put into stack

--- src/components/ProgressView/ProgressView.stories.js
+++ src/components/ProgressView/ProgressView.stories.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 // import { withKnobs, radios, number } from '@storybook/addon-knobs';
+import Accordion from 'react-bootstrap/Accordion';
 import ProgressView from '.';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const testRef = 'waltham_waterfall';
 
-const stories = storiesOf('ProgressGallery', module);
+const stories = storiesOf('ProgressView', module);
 // stories.addDecorator(withKnobs);
 
 stories.add('Default', () => (
   <div className="story">
-    <ProgressView refKey={testRef} />
+    <Accordion>
+      <ProgressView refKey={testRef} />
+    </Accordion>
   </div>
   
 ))

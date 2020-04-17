733099487c18635cd3b32b9acd33190369dfb622
Charlie Kelly <charliekelly31415@gmail.com>
2020-04-17 01:48:09 -0400

    use masonry instead of cropped thumbs

---
+++ src/components/MasonGallery/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Gallery from 'react-photo-gallery';
+import { masonryImageArray } from '../../utils/imageApi';
+
+const MasonGallery = props => {
+  const { galleryImages, scaleFactor } = props;
+  const images = masonryImageArray(galleryImages, scaleFactor);
+  console.log('gallery images: ', images);
+  return (
+    <Gallery photos={images} />
+  )
+};
+
+MasonGallery.propTypes = {
+  /**
+   * the image array (results of fetchGallery)
+   */
+  galleryImages: PropTypes.array.isRequired,
+  /**
+   * what to do when image is selected
+   */
+  selectThumbnail: PropTypes.func,
+  /**
+   * index
+   */
+  imageIndex: PropTypes.number,
+  /**
+   * amount to scale images (if used, should be < 1)
+   */
+  scaleFactor: PropTypes.number
+};
+
+MasonGallery.defaultProps = {
+  galleryImages: [],
+  imageIndex: 0,
+  scaleFactor: 1
+}
+
+export default MasonGallery;

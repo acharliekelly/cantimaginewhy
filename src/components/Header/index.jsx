c8b5ebed934dd086eb8327c2fe5615ef008c3176
Charlie Kelly <charliekelly31415@gmail.com>
2020-04-17 01:38:31 -0400

    replace BS collapse with socks breakpoints

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,26 +4,35 @@ import Container from 'react-bootstrap/Container';
 import ContactLinks from '../ContactLinks';
 import Logo from '../Logo/';
 import Menu from '../Menu';
+import { Breakpoint } from 'react-socks';
 
 const Header = props => (
   <div className="menu-wrapper">
-    <Navbar bg="light" className="justify-content-between" expand="md">
-      <Navbar.Brand>
-        <Logo {...props} />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="menu-content" />
-      <Navbar.Collapse id="menu-content">
-        <Container className="justify-content-end">
-          <Menu {...props} />
-        </Container>
-        <Container className="justify-content-end">
-          <ContactLinks 
-            displayType="icon" 
-            size="2x" 
-            horizontal="sm" 
-            group="head" />
-        </Container>
-      </Navbar.Collapse>
+    <Navbar bg="light" className="justify-content-between">
+      <Breakpoint md up>
+        <Navbar.Brand>
+          <Logo {...props} />
+        </Navbar.Brand>
+      </Breakpoint>
+      
+
+      <Container className="justify-content-end">
+        <Breakpoint md down>
+          <Menu type="drop" {...props} />
+        </Breakpoint>
+        <Breakpoint lg up>
+          <Menu type="nav" {...props} />
+        </Breakpoint>
+      </Container>
+        
+      <Container className="justify-content-end">
+        <Breakpoint sm down>
+          <ContactLinks displayType="icon" size="lg" group="head" />
+        </Breakpoint>
+        <Breakpoint md up>
+          <ContactLinks displayType="icon" size="2x" horizontal="lg" group="head" />
+        </Breakpoint>
+      </Container>
     </Navbar>
   </div>
 );

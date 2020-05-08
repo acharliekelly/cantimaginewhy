f3bdc16c56a6fafddd49bc23193542beae517a87
Charlie Kelly <charliekelly31415@gmail.com>
2020-05-07 22:21:23 -0400

    enabled property

--- src/components/higherOrder/withStacking.jsx
+++ src/components/higherOrder/withStacking.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
 
+
 // HOC for putting component into Stack
 export function withStacking(WrappedComponent) {
   return class extends React.Component {
     
     render () {
-      const { eventKeyName, cardTitle, variant, maxHeight, ...passThroughProps} = this.props;
+      const { eventKeyName, cardTitle, variant, maxHeight, enabled, ...passThroughProps} = this.props;
 
       return (
         <Card className="stacked-card" border={variant} text={variant}>
-          <Accordion.Toggle as={Card.Header} eventKey={eventKeyName} style={{cursor: 'pointer'}}>
+          <Accordion.Toggle as={Card.Header} eventKey={eventKeyName} disabled={!enabled} style={{cursor: 'pointer'}}>
             <strong>{cardTitle}</strong>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey={eventKeyName}>

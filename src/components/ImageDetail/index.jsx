992f04c716cab3dcbdaccb2fe7469afeb4614847
Charlie Kelly <charliekelly31415@gmail.com>
2020-05-11 11:52:05 -0400

    simplify by using gallery

--- src/components/ImageDetail/index.jsx
+++ src/components/ImageDetail/index.jsx
@@ -1,82 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+// import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
-import ImageToolbar from '../ImageToolbar/';
+import ImageToolbar, { TouchToolbar } from '../ImageToolbar/index';
 import { withLightbox } from '../higherOrder/withLightbox';
-import DisplayImage from '../DisplayImage';
+import DisplayImage, { DisplayImageZoom } from '../DisplayImage';
 import ImageInfo from '../ImageInfo';
-import classNames from 'classnames';
+// import classNames from 'classnames';
+import { Breakpoint } from 'react-socks';
 
 import './detail.scss';
 
 const ImageDetail = props => {
-  const { imageList, imageIndex, isFullWidth, imageMovement } = props;
-  const { moveNext, movePrevious } = imageMovement;
+  const { imgGallery } = props;
+ 
 
-  const [ currentImage, setCurrentImage ] = useState(null);
-  useEffect(() => {
-    if (imageList)
-      setCurrentImage(imageList[imageIndex]);
-  }, [imageList, imageIndex]); 
-
-  const magnifyImage = () => {
-    props.selectLightbox(currentImage.public_id, imageList);
-    
-  }
-
-  if (currentImage) {
-    const cls = classNames('image-detail', { 'full-width': isFullWidth });
+  if (imgGallery.currentImage) {
     return (
-      <Container className={cls}>
-        <DisplayImage currentImage={currentImage} imageZoom={isFullWidth ? null:  magnifyImage} {...props} />
-        
-        <ImageToolbar 
-            variant="light"
-            imgSize="2x"
-            fullWidth
-            prevImageFn={movePrevious}
-            nextImageFn={moveNext}
-            zoomImageFn={isFullWidth ? null:  magnifyImage}
-            disableCarousel={imageList.length < 2}
-          />
-        
-          <ImageInfo currentImage={currentImage} />
-
-      </Container>    
+      <>
+      <Breakpoint md down>
+        <Container className="image-detail full-width">
+          <DisplayImageZoom {...props} />
+          <TouchToolbar {...props} />
+          <ImageInfo currentImage={imgGallery.currentImage} />
+        </Container>
+      </Breakpoint>
+      <Breakpoint lg up>
+        <Container className="image-detail">
+          <DisplayImage {...props} />
+          <ImageToolbar variant="light" imgSize="2x" {...props} />
+          <ImageInfo currentImage={imgGallery.currentImage} />
+        </Container>
+      </Breakpoint>
+      </>
     )
   } else {
     return (
       <div className="no-data"></div>
     )
   }
-}
-
-ImageDetail.propTypes = {
-  /**
-   * results of fetchGallery
-   */
-  imageList: PropTypes.array,
-  /**
-   * the starting index
-   */
-  imageIndex: PropTypes.number,
-  /**
-   * the function to move back
-   */
-  movePrevious: PropTypes.func,
-  /**
-   * the function to move forward
-   */
-  moveNext: PropTypes.func,
-  imageMovement: PropTypes.object,
-  /**
-   * the function to open image in Zoom
-   */
-  selectLightbox: PropTypes.func,
-}
-
-ImageDetail.defaultProps = {
-  imageIndex: 0,
 }
 
 export default withLightbox(ImageDetail);

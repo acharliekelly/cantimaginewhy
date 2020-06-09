ca7fffe56394f3ff9e3c800640a8a35e78deefb8
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-09 02:47:34 -0400

    move menu to simulated JSON file

---
+++ src/config/menu.js
@@ -0,0 +1,30 @@
+/**
+ * Page object:
+ * - name: {string} Display Name
+ * - location: {string} relative path
+ * - external: {boolean} external site
+ */
+
+export const pages = [
+  {
+    name: "Home",
+    location: "/"
+  },
+  {
+    name: "Artwork",
+    location: "artwork"
+  },
+  {
+    name: "About",
+    location: "about",
+  },
+  {
+    name: "Connect",
+    location: "connect"
+  },
+  {
+    name: "Shop",
+    location: "https://charlie-kelly.pixels.com",
+    external: true
+  }
+];

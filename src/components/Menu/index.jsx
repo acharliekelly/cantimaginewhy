866212490e10ff0c468a3d144b6cf0ee420a453c
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-11 02:09:20 -0400

    extended menu component

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,63 +3,13 @@ import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
 import Nav from 'react-bootstrap/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import ExternalLink from '../Buttons/ExternalLink';
+import { itemDisplayTypes } from '../../utils/constants';
 
 import './menu.scss';
 
-const isActive = ({ isCurrent }) => {
-  return isCurrent ? { className: 'active' } : {}
-}
-
-const isChildActive = ({ isPartiallyCurrent }) => {
-  return isPartiallyCurrent ? { className: 'active' } : {}
-}
-
-const MenuLink = props => {
-  const { item, subMenu, icons } = props;
-  let location = item.location;
-  let activeFn = isActive;
-  if (subMenu) { // section link
-    location = `../${location}`;
-  } else if (location !== '/') { // top-level page link, not Home
-    activeFn = isChildActive;
-  }
-  return (
-    <Link getProps={activeFn} to={location}>
-      {icons && <FontAwesomeIcon icon={item.icon}/>} {item.name}
-    </Link>
-  )
-
-}
-
-
-const Menu = props => {
-  const { items, navClass, subMenu, icons } = props;
-  return (
-    <Nav className={navClass}>
-      {items.map((item, index) => (
-        <Nav.Item key={index}>
-          {item.external ? externalPage(item) : (
-            <MenuLink subMenu={subMenu} item={item} icons={icons} />
-          )}
-        </Nav.Item>
-      ))}
-    </Nav>
-  )
-}
-
-Menu.propTypes = {
-  items: PropTypes.array.isRequired,
-  navClass: PropTypes.string,
-  subMenu: PropTypes.bool,
-  icons: PropTypes.bool
-}
-
-Menu.defaultProps = {
-  navClass: 'menu-nav',
-  subMenu: false,
-  icons: false
-}
+// TODO: make this better
 
 const externalPage = item => (
   <ExternalLink 
@@ -70,6 +20,106 @@ const externalPage = item => (
       {item.name}
   </ExternalLink>
 );
+
+const isActive = ({ isCurrent }) => {
+  return isCurrent ? { className: 'active' } : {}
+}
+
+const isChildActive = ({ isPartiallyCurrent }) => {
+  return isPartiallyCurrent ? { className: 'active' } : {}
+}
+
+const MenuLink = props => {
+  const { item, subMenu, displayStyle, minimize } = props;
+  const display = minimize ? itemDisplayTypes.iconOnly : displayStyle;
+  let location = item.location;
+  let activeFn = isActive;
+
+  if (subMenu) { // section link
+    location = `../${location}`;
+  } else if (location !== '/') { // top-level page link, not Home
+    activeFn = isChildActive;
+  }
+
+  return (
+    <Link getProps={activeFn} to={location}>
+      {menuContent(item, display)}
+    </Link>
+  )
+  
+}
+
+
+const menuContent = (item, displayStyle) => {
+  switch (displayStyle) {
+    case itemDisplayTypes.iconOnly:
+      return <FontAwesomeIcon icon={item.icon} title={item.name} />
+    case itemDisplayTypes.textOnly:
+      return <span className="link-text">{item.name}</span>
+    default: // icon and text
+      return (
+        <>
+          <FontAwesomeIcon icon={item.icon} title={item.name} />
+          <span className="link-text">{item.name}</span>
+        </>
+      )
+  }
+}
+
+
+const Menu = props => {
+  const { items, navClass, minimize } = props;
+  let list = items;
+  if (minimize) {
+    list = items.filter(item => !item.external)
+  }
+  
+  return (
+    <Nav className={navClass}>
+      {list.map((item, index) => (
+        <Nav.Item key={index} className={classNames({'icon': minimize})}>
+          {item.external ? externalPage(item) : (
+            <MenuLink item={item} {...props} />
+          )}
+        </Nav.Item>
+      ))}
+    </Nav>
+  )
+}
+
+Menu.propTypes = {
+  /**
+   * the list of menu items
+   */
+  items: PropTypes.array.isRequired,
+
+  /**
+   * the className of the nav element
+   */
+  navClass: PropTypes.string,
+
+  /**
+   * not top-level menu
+   */
+  subMenu: PropTypes.bool,
+
+  /**
+   * itemDisplayTypes
+   */
+  displayStyle: PropTypes.number,
+
+  /**
+   * as small as possible
+   */
+  minimize: PropTypes.bool
+}
+
+Menu.defaultProps = {
+  navClass: 'menu-nav',
+  subMenu: false,
+  displayStyle: itemDisplayTypes.textOnly,
+  minimize: false
+}
 
 
 export default Menu;

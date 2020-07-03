51948321ea05ec8590dfaa22f5020fe42daa5e48
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-03 12:13:39 -0400

    reach-based router

--- src/routers/PageRouter.jsx
+++ src/routers/PageRouter.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import ArtRouter from './ArtRouter';
+// import ArtRouter from './ArtRouter';
 import ConnectPage from '../pages/Connect';
 import { HomePage } from '../pages/Home';
 import { AboutPage, AboutSection } from '../pages/About';
+import ArtworkPage from '../pages/Artwork';
 
+const FilterPage = props => (
+  <ArtworkPage view="filter" {...props} />
+)
 
 /**
  * Reach router Page navigation
@@ -14,12 +18,24 @@ const PageRouter = props => (
     <HomePage path="/" />
     <AboutPage path="about" />
     <AboutSection path="about/:sectionId" />
-      
+
     
+      
     <ConnectPage path="connect" />
     <ConnectPage path="connect/:sectionId" />
+    
 
-    <ArtRouter path="artwork/*" />
+    <ArtworkPage path="artwork">
+      <ArtworkPage path="albums" />
+      <ArtworkPage path="album/:album" />
+      
+    </ArtworkPage>
+    
+    
+    <FilterPage path="artwork/filters" />
+    <FilterPage path="artwork/filters/:index" />
+    <FilterPage path="artwork/filter/:filter" />
+    
 
   </Router>
 );

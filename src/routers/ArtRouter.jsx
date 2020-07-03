26a37fd8673bcb75c8957768ff55253e7fe7c614
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-03 12:13:16 -0400

    router for artwork (WIP)

--- src/routers/ArtRouter.jsx
+++ src/routers/ArtRouter.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import withSizes from 'react-sizes';
-import { mapSizesToProps } from '../utils/system';
-import ArtworkPage from '../pages/Artwork';
-import MobilePage from '../pages/Mobile';
+
 
 const ArtRouter = props => (
   <Router>
-    {props.isDesktop ? (
-      <ArtworkPage path="/*" />
-    ) : (
-      <MobilePage path="/*" />
-    )}
+    
   </Router>
 )
 
-export default withSizes(mapSizesToProps)(ArtRouter);
+export default ArtRouter;

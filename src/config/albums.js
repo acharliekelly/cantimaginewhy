0e5c7242dc8d9342584725fa2472aa6f48d33fe2
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-03 12:10:44 -0400

    add display field

--- src/config/albums.js
+++ src/config/albums.js
@@ -18,7 +18,8 @@ export const albums = [
     "thumbnail": "art/a_friday_in_september",
     "description": "Painted on site",
     "sortField": ".completed",
-    "sortDir": "desc"
+    "sortDir": "desc",
+    "display": true
   },
   {
     "name": "Scenery",
@@ -26,7 +27,8 @@ export const albums = [
     "thumbnail": "art/midwinter",
     "description": "From a photograph I took",
     "sortField": ".year",
-    "sortDir": "desc"
+    "sortDir": "desc",
+    "display": true
   },
   {
     "name": "Landscape",
@@ -34,7 +36,8 @@ export const albums = [
     "thumbnail": "nfs/the_cliffs",
     "description": "From a photograph somebody took",
     "sortField": ".year",
-    "sortDir": "desc"
+    "sortDir": "desc",
+    "display": false
   },
   {
     "name": "Sketch",
@@ -42,42 +45,48 @@ export const albums = [
     "thumbnail": "nfs/davis-t",
     "description": "Onsite pencil sketches of local landmarks",
     "sortField": ".completed",
-    "sortDir": "desc"
+    "sortDir": "desc",
+    "display": true
   },
   {
     "name": "Name Tags",
     "tag": "nametag",
     "thumbnail": "nfs/charlie-7_37671079756_o_fvboho",
     "description": "Name tags I made for various people, including me",
-    "sortField": ".year"
+    "sortField": ".year",
+    "display": true
   },
   {
     "name": "Name Plates",
     "tag": "illumination",
     "thumbnail": "nfs/emrys_2011",
     "description": "Like name tags, but bigger",
-    "sortField": ".alb-order"
+    "sortField": ".alb-order",
+    "display": true
   },
   {
     "name": "Knotwork",
     "tag": "knotwork",
     "thumbnail": "art/lotus",
     "description": "Celtic-inspired artwork",
-    "sortField": ".caption"
+    "sortField": ".caption",
+    "display": true
   },
   {
     "name": "Cards",
     "tag": "cards",
     "thumbnail": "art/st-patricks-day-card-2011",
     "description": "Designs for various holidays and special occaisions",
-    "sortField": '.year'
+    "sortField": '.year',
+    "display": true
   },
   {
     "name": "AANE",
     "tag": "aane",
     "thumbnail": "nfs/golden",
     "description": "A series of the letters A-A-N-E, arranged artistically",
-    "sortField": ".alb-order"
+    "sortField": ".alb-order",
+    "display": false
   },
   {
     "name": "People",
@@ -85,20 +94,31 @@ export const albums = [
     "thumbnail": "nfs/rihanna",
     "description": "Sketches of people; some of them famous",
     "sortField": ".year",
-    "sortDir": "desc"
+    "sortDir": "desc",
+    "display": true
   },
   {
     "name": "Plaitwork",
     "tag": "plaitwork",
     "thumbnail": "nfs/lattice_mandelbrot-set",
     "description": "My first type of artwork; like knotwork, but with all right angles. Looks like placemats.",
-    "sortField": ".completed"
+    "sortField": ".completed",
+    "display": true
   },
   {
     "name": "Archive",
     "tag": "archive",
     "thumbnail": "nfs/ferry-beach-rock-painting_41932404964_o_lf4uhn",
-    "description": "All the other stuff I've made"
+    "description": "All the other stuff I've made",
+    "display": true
+  },
+  {
+    "name": "Logos",
+    "tag": "cant-imagine",
+    "thumbnail": "icon/ciw9",
+    "description": "Logos designs",
+    "sortField": ".completed",
+    "display": false
   }
 ];
 

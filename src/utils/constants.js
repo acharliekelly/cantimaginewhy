a48ed5bdba376fa11917e14ea649d5d8358cb370
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-29 00:53:40 -0400

    universal constants

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+
+// constants for general use
+export const ALBUM_MODE = 'ALBUM';
+export const FILTER_MODE = 'FILTER';
+export const NAVIGATION_MODES = [
+  ALBUM_MODE,
+  FILTER_MODE
+];
+
+export const MAIN_CONTEXT = 'MAIN';
+export const PROGRESS_CONTEXT = 'PROGRESS';
+export const GALLERY_CONTEXTS = [
+  MAIN_CONTEXT,
+  PROGRESS_CONTEXT
+]

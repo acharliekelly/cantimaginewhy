b795a8af7c6e30635f6e1d35f4bd95caa87b8187
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-29 17:37:52 -0400

    avoid relative paths with resolvers

--- .webpack.config.js
+++ .webpack.config.js
@@ -6,7 +6,7 @@ var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-    entry: APP_DIR + '/index.jsx',
+    entry: APP_DIR + '/index.js',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
@@ -18,7 +18,20 @@ var config = {
                 include : APP_DIR,
                 loader : 'babel'
             }
-        ]
+        ],
+        resolve: {
+            alias: {
+                ActionTypes$: path.resolve(__dirname, 'src/redux/actions/actionTypes.js'),
+                Api: path.resolve(__dirname, 'src/api/'),
+                Comps: path.resolve(__dirname, 'src/components/'),
+                Constants$: path.resolve(__dirname, 'src/utils/constants.js'),
+                Containers: path.resolve(__dirname, 'src/containers/'),
+                LocalData: path.resolve(__dirname, 'src/json/'),
+                Redux: path.resolve(__dirname, 'src/redux/'),
+                Style: path.resolve(__dirname, 'src/scss/'),
+                Utils: path.resolve(__dirname, 'src/utils/'),
+            }
+        }
     }
 };
 

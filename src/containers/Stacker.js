6c295c1c3b5d1ca7d33ad87eea96ccd606d2ac8e
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-29 23:36:31 -0400

    tweak

--- src/containers/Stacker.js
+++ src/containers/Stacker.js
@@ -1,25 +1,33 @@
 import { connect } from 'react-redux';
 import {
   selectGallery,
-  fetchMainGallery
+  fetchMainGallery,
+  fetchAlbumExplanation,
+  fetchProgressGallery,
+  fetchGeoData
 } from '../redux/actions';
 import { INITIAL_STATE } from '../redux/reducers';
+import { getThumbnailSize } from 'Utils/imageUtils';
 import Stacker from '../components/Stacker';
 
 const mapStateToProps = (state = INITIAL_STATE) => ({
   tagObject: state.navigator.selectGallery, 
   referenceKey: state.imageDetail.referenceKey, 
   isFullWidth: state.viewPort <= 500,
-  galleryImages: state.primaryGallery.imageList
+  galleryImages: state.primaryGallery.imageList,
+  thumbSize: getThumbnailSize(state.primaryGallery.imageList.length)
 });
 
 const updateSelectNav = tagName => {
   selectGallery(tagName);
   fetchMainGallery(tagName);
+  fetchAlbumExplanation(tagName);
 }
 
 const actionCreators = {
-  updateSelectNav
+  updateSelectNav,
+  fetchProgressGallery,
+  fetchGeoData
 };
 
 export default connect(mapStateToProps, actionCreators)(Stacker);

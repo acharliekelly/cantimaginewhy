ba131c78931a8260ee7051cabadd266e8921179e
Charlie Kelly <charliekelly31415@gmail.com>
2020-08-02 00:48:53 -0400

    improved

--- src/containers/ImageDetail.js
+++ src/containers/ImageDetail.js
@@ -1,11 +1,11 @@
-import * as reactRedux from 'react-redux';
+import { connect } from 'react-redux';
 import { INITIAL_STATE } from '../redux/reducers/initialStateTree';
 import { 
-  selectImage,
   openLightbox,
-  closeLightbox 
+  closeLightbox,
+  nextImage,
+  prevImage 
 } from '../redux/actions/';
-import { MAIN_CONTEXT } from '../utils/constants';
 import ImageDetail from '../components/ImageDetail';
 
 const mapStateToProps = (state = INITIAL_STATE, ownProps) => ({
@@ -14,16 +14,6 @@ const mapStateToProps = (state = INITIAL_STATE, ownProps) => ({
   isFullWidth: ownProps.isFullWidth
 });
 
-const nextImage = (imageList, currentIndex) => {
-  const next = (currentIndex + 1) % imageList.length;
-  selectImage(MAIN_CONTEXT, next);
-}
-
-const prevImage = (imageList, currentIndex) => {
-  const prev = (currentIndex + imageList.length - 1) % imageList.length;
-  selectImage(MAIN_CONTEXT, prev);
-}
-
 const actionCreators = {
   nextImage,
   prevImage,
@@ -31,5 +21,5 @@ const actionCreators = {
   closeLightbox
 }
 
-export default reactRedux.connect(mapStateToProps, actionCreators)(ImageDetail);
+export default connect(mapStateToProps, actionCreators)(ImageDetail);
 
--- src/containers/ImageInfo.js
+++ src/containers/ImageInfo.js
@@ -1,10 +1,22 @@
 import { connect } from 'react-redux';
-import { } from '../redux/actions';
+import { StateLocator } from '../utils/constants';
 import { INITIAL_STATE } from '../redux/reducers/initialStateTree';
 import ImageInfo from '../components/ImageInfo';
+import { contextFieldLabels } from '../utils/imageContext';
+
+
+export const initialData = {
+  locator: StateLocator.IMAGE_DETAIL,
+  caption: '',
+  description: '',
+  medium: '',
+  size: '',
+  completedOn: null
+};
 
 const mapStateToProps = (state = INITIAL_STATE) => ({
-  currentImage: state.imageDetail
+  imageData: state.currentImage.rawData.context.custom,
+  contextFieldLabels
 });
 
 export default connect(mapStateToProps)(ImageInfo);

c59c18e2df5aff2ee9bbe4354ee184465a3b805a
Charlie Kelly <charliekelly31415@gmail.com>
2020-08-02 00:47:37 -0400

    simplify

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -1,13 +1,10 @@
 import { connect } from 'react-redux';
 import AboutPage from '../components/AboutPage';
-import { aboutContent } from '../json/text';
 
 import { INITIAL_STATE } from '../redux/reducers/initialStateTree';
 
 const mapStateToProps = (state = INITIAL_STATE) => ({
-  isFetching: false,
-  contentText: aboutContent,
-  error: null
+  aboutInfo: state.aboutInfo
 });
 
 export default connect(mapStateToProps)(AboutPage);

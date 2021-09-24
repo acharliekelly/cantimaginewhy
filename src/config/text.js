0932bde85be6fb594372b6a939620f01b94b0615
Charlie Kelly <charliekelly31415@gmail.com>
2021-09-23 21:06:32 -0400

    remove block of about text

--- src/config/text.js
+++ src/config/text.js
@@ -172,14 +172,6 @@ export const aboutContent = {
     and then into representational art. First plants and random objects, then people, and then landscapes and architecture. 
     I also evolved from graphite and colored pencil to watercolors to acrylics.`,
 
-    `Because of where I learned to draw, however, I developed some unusual artistic styles based on the materials 
-    that were available. For example, although watercolor paint was available, the same cannot be said for the 
-    heavyweight, absorbent paper that is typically used by watercolor painters in the rest of the world. 
-    As I quickly learned, applying watercolor to regular paper will cause it to curl up and become misshapen. 
-    The only way to prevent this was to apply the pigment in tiny, non-contiguous dots. It turns out this is actually 
-    a style of painting, called Pointillism. It was championed by the French post-impressionist Georges Seurat. But 
-    I didn’t realize it at the time; I was just trying to paint without ruining the paper.`,
-
     `At present, I mostly do landscapes. When the weather is nice, I prefer to paint my landscapes 
     "en plein air", which means standing in front of the landscape while I'm painting it.`
   ],
